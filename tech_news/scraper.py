645dccd1ae59aeacbd27f8ae6b536beade2b3a96
Andre Menezes <dehhmenezes87@gmail.com>
2022-01-11 20:50:23 -0300

    Requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,37 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = (
+        selector.css(".z--font-bold").css("*::text").get().strip() or " ".get()
+    )
+    try:
+        shares_count = selector.css(".tec--toolbar__item::text").get()\
+            .strip().split(" ")[0]
+    except AttributeError:
+        shares_count = 0
+    comments_count = int(
+        selector.css(".tec--toolbar__item > button ::attr(data-count)").get()
+    )
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories a ::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
