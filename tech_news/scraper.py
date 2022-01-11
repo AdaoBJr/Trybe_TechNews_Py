b3e3466e6e9d564ac799297a35972d16416e0a06
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-11 14:24:24 -0300

    requisito 1 completo

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
@@ -21,3 +32,6 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+print(fetch("https://httpbin.org/delay/5"))
