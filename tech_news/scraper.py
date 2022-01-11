9ef6145a921ea77651d1302fdbabc648c738cb61
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-11 19:03:46 -0300

    feat: scape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -21,13 +21,16 @@ def scrape_novidades(html_content):
     result = selector.css(
         ".tec--list--lg .tec--card__title > a ::attr(href)"
     ).getall()
-
     return result
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = selector.css(
+        ".tec--list.tec--list--lg > a ::attr(href)"
+    ).get()
+    return result
 
 
 # Requisito 4
