cf592e92352e812bc32c8481e4302161889389a7
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-11 18:36:11 -0300

    feat: fetch(url)

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        res = requests.get(url, timeout=3)
+        if not res.status_code == 200:
+            return None
+        return res.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
