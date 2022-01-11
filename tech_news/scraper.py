d28685c6a28934644f4d25ba6360769290be28b8
Lara-Capila <laracapilak@gmail.com>
2022-01-11 19:08:14 -0300

    feat(req 1): Crie a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,24 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=2)
+        # response.raise_for_status() Não deu bom!
+        # https://stackoverflow.com/questions/28377421/why-do-i-receive-a-timeout-error-from-pythons-requests-module
+        if response.status_code == 200:
+            return response.text
+    except requests.exceptions.Timeout:
+        return None
+    # não entendi esse else, aula do Benites 10A
+    # else:
+    #     if response.status_code == 200:
+    #         return response.text
+    # return None
 
 
 # Requisito 2
