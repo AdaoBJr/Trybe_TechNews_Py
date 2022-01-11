591f7de525f4cb60f558949383adb276c6dacb0f
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-11 15:14:42 -0300

    requisito 2 e 3 feitos

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 # Requisito 1
 
@@ -16,12 +17,16 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content) 
+    news_list = selector.css('.tec--list--lg h3.tec--card__title > a ::attr(href)').getall()
+    return news_list
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content) 
+    next_page_link = selector.css('#js-main > div > div > div.z--col.z--w-2-3 > div.tec--list.tec--list--lg > a::attr(href)').get()
+    return next_page_link
 
 
 # Requisito 4
@@ -32,6 +37,3 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-print(fetch("https://httpbin.org/delay/5"))
