b82fd85a7fbfcaa46c0aa0502a06f6fdce56a280
isTorres <ismaeltorres612@gmail.com>
2022-01-11 19:40:19 -0300

    feat: reqs 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,38 @@
+import requests
+from parser import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        res = requests.get(url, timeout=3)
+        res.raise_for_status()
+    except requests.HTTPError:
+        return None
+    except requests.Timeout:
+        return None
+    finally:
+        if res.status_code != 200:
+            return None
+        else:
+            return res.text
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    res = requests.get(html_content)
+    selector = Selector(text=res.text)
+    links = []
+    news_list = selector.css("div:nth-child(1) > article > div > h3 > a")
+    try:
+        for link in news_list:
+            path = link.attrib["href"]
+            links.append(f"{url}{path}")
+    except KeyError:
+        return []
+
+    return links
 
 
 # Requisito 3
@@ -16,6 +43,7 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    print(html_content)
 
 
 # Requisito 5
