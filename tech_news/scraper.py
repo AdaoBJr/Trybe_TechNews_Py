87aa27e79ceae130a28ee37a880a251d4d0dfab2
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-11 16:01:43 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+
+    if(response.status_code == 200):
+        return response.text
 
 
 # Requisito 2
