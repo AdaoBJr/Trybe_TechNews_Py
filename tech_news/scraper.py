be803aa4ba59c799863e0a7ee6789eb536030599
Leandro Reis <lleandrofr@gmail.com>
2022-01-11 19:06:19 -0300

    req 1: implement scraper function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    """Hi :D"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if not response.status_code == 200:
+            return None
+        return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
