d3e2e016f80c75d7985d3043f3559434a9c75e69
Mariana Mohr <marianamohr@gmail.com>
2022-01-11 16:06:23 -0300

    Req 1 - feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        # fazendo uma requisição e esperando no max 3 seg
+        time.sleep(1)
+        res = requests.get(url, timeout=2)
+    except requests.exceptions.RequestException:
+        return None
+    else:
+        # Testabdo se a comunicação com o site está ok, retorna o texto
+        if res.status_code == 200:
+            return res.text
+    return None
 
 
 # Requisito 2
