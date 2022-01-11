044bc9ee170d20dc65a2b1e2974297402e908e59
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2022-01-11 15:23:52 -0300

    req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,19 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
-    # alteração pra commit
+    sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
