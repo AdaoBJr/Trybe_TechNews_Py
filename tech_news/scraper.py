53e646cc22c865d99fec412d954bd9996b4f35ae
Mariana Mohr <marianamohr@gmail.com>
2022-01-11 19:17:51 -0300

    Req 4 - feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -8,7 +8,7 @@ def fetch(url):
     try:
         # fazendo uma requisição e esperando no max 3 seg
         time.sleep(1)
-        res = requests.get(url, timeout=2)
+        res = requests.get(url, timeout=3)
     except requests.exceptions.RequestException:
         return None
     else:
@@ -36,15 +36,46 @@ def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     # faz a busca no arquivo pela classe css,
     # encontrando o pai e depois disso a tag q possui o atr href
-    result = selector.css(
-        ".z--container tec--btn::attr(href)"
-    ).getall()
+    result = selector.css(".tec--btn::attr(href)").get()
     return result
 
 
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    # zozimo, arlen e alessandra
+    url = selector.css("link[rel=canonical] ::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("time ::attr(datetime)").get()
+    writer = selector.css(
+        ".z--font-bold").css("*::text").get().strip() or " "
+
+    try:
+        shares_count = selector.css(
+            ".tec--toolbar__item::text"
+        ).get().strip().split(" ")[0]
+    except AttributeError:
+        shares_count = 0
+    comments_count = selector.css(".tec--btn::attr(data-count)").get()
+    summary_select = "".join(selector.css(
+        ".tec--article__body > p:nth-child(1) ::text"
+    ).getall())
+    sources = selector.css(".z--mb-16 .tec--badge ::text").getall()
+
+    categories = selector.css(".tec--badge--primary::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary_select,
+        "sources": [source.strip() for source in sources],
+        "categories": [categories.strip() for categories in categories],
+    }
 
 
 # Requisito 5
