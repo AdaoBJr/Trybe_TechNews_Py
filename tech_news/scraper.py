56191ad7a1a3e548e410f3031e65dee20ee2c9c2
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-11 11:22:58 -0300

    req 1 - testando req1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code != 200:
+            return None
+
+        return response.text
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
