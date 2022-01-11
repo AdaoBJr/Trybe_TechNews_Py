ab98be047428c36005cb36e5ac13268ed436fb89
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-11 18:58:00 -0300

    Feat: Resolvendo requisito do 1 ao 3 do projeto Tech News

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,21 +1,50 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+        return response.text
+    except requests.HTTPError:
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    response = selector.css(
+        ".tec--list--lg .tec--card__title > a ::attr(href)"
+    ).getall()
+    return response
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    response = selector.css("div.tec--list.tec--list--lg "
+                            "> a ::attr(href)").get()
+    return response
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head > link[rel=canonica] ::attr(href)").get()
+    title = selector.css("").get()
+    timestamp = selector.css("").get()
+    writer = selector.css("").get()
+    shares_count = selector.css("").get()
+    comments_count = selector.css("").get()
+    summary = selector.css("").get()
+    sources = selector.css("").getall()
+    categories = selector.css("").getall()
 
 
 # Requisito 5
