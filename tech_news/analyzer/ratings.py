0d332065e75e8d05493e92e1c1f7caee4dc4e2ad
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2022-01-12 17:22:09 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    news_list = find_news()
+    result = []
+
+    for news in news_list:
+        news["count"] = news["shares_count"] + news["comments_count"]
+
+    news_list.sort(key=lambda x: x["count"], reverse=True)
+    top_five = news_list[:5]
+
+    for news in top_five:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 11
