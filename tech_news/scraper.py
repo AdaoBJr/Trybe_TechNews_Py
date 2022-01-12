3036a829af1b10da0b8d014a793734bcc0ec273d
Gabriel <gabriel.mirandab92@gmail.com>
2022-01-12 18:10:32 -0300

    adding scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_url = selector.css(".tec--btn::attr(href)").get()
+    return news_url
 
 
 # Requisito 4
