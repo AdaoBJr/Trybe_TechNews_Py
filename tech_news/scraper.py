d70c77c7ca8aa8d6712480079f2ec795b9356c51
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-12 19:26:04 -0300

    Inserir - scraper.py - Criação da função que retorna a url de proxima pagina e url de noticias

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,5 @@
 import requests
+from parsel import Selector
 import time
 from requests.exceptions import ReadTimeout
 
@@ -17,19 +18,34 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    if html_content == "":
+        return []
+    seletor = Selector(html_content).css(
+        'h3 .tec--card__title__link::attr(href)').getall()
+    return seletor
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    seletor = Selector(html_content).css(
+        "div.tec--list--lg a.tec--btn--lg::attr(href)").get()
+    return seletor
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    result = []
+    if html_content == "":
+        return result
+    seletor = Selector(html_content).css(
+        '.tec--card__title__link::attr(href)').getall()
+    for item in seletor:
+        result.append(item)
+    return result
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
