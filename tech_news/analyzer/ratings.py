1128aafeb997edd8d9887d834779d5f665ef07ac
Huggo Parcelly <h.parcelly@gmail.com>
2022-01-12 14:26:29 -0300

    adiciona a funçao que retorna as 5 noticias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,33 @@
+from tech_news.analyzer.search_engine import search_by_title
+from tech_news.database import find_news
+
+
+def ordenacao_popularidade(news_dict):
+    title_list = []
+    # ordenação decrescente de titulos pela popularidade
+    # realizado com pesquisa em 
+    # https://diegomariano.com/como-ordenar-um-dicionario-em-python/
+    for title in sorted(news_dict, key=news_dict.get, reverse=True):
+        title_list.append(title)
+    return title_list
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_dict = {}
+    news_list = []
+    all_news = find_news()
+    # Cria um dicionário onde a chave é titulo e o valor a popularidade
+    for news in all_news:
+        soma = 0
+        soma += news["shares_count"] + news["comments_count"]
+        news_dict[news["title"]] = soma
+
+    title_list = ordenacao_popularidade(news_dict)
+    for title in title_list[:5]:
+        news_list.extend(search_by_title(title))
+
+    return news_list
 
 
 # Requisito 11
