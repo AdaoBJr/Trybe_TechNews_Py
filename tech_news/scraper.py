22bed838691ef96ccb62321492d8f00e8cd63020
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-12 19:12:00 -0300

    req 4 e 5 - feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,49 @@
 import requests
 from parsel import Selector
 from time import sleep
+from tech_news.database import create_news
+
+
+def retirar_espaco(lista):
+    new_list = []
+    for i in lista:
+        if not i == " ":
+            new_list.append(i.strip())
+
+    return new_list
+
+
+def check_string(valor):
+    if valor is None:
+        return None
+    return valor.strip()
+
+
+def check_comments(valor):
+    if valor is None:
+        return int(0)
+    return int(valor)
+
+
+def check_count(valor):
+    if valor is None:
+        return int(0)
+
+    result = valor.strip().split(" ")[0]
+    return int(result)
+
+
+def check_writer(html_content):
+    select = Selector(html_content)
+    selectors = [
+        "p.z--m-none.z--truncate.z--font-bold *::text",
+        "div.tec--timestamp__item.z--font-bold > a ::text",
+        "div.tec--article__body.p402_premium > p:nth-child(1) ::text",
+    ]
+    for selec in selectors:
+        result = select.css(selec).get()
+        if result is not None:
+            return check_string(result)
 
 
 # Requisito 1
@@ -34,24 +77,51 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     select = Selector(html_content)
+    data = {}
 
-    url = html_content
-    title = select.css("#js-article-title ::text").get()
-    timestamp = select.css("#js-article-date ::attr(datetime)").get()
-    writer = (
-        select.css("p.z--m-none.z--truncate.z--font-bold a::text")
-        .get()
-        .strip()
+    data["url"] = select.css("head > link[rel=canonical] ::attr(href)").get()
+    data["title"] = select.css("#js-article-title ::text").get()
+    data["timestamp"] = select.css("#js-article-date ::attr(datetime)").get()
+    data["writer"] = check_writer(html_content)
+    data["shares_count"] = check_count(
+        select.css("nav.tec--toolbar > div::text").get()
     )
-    shares_count = select.css("svg.feather.z--mr-8 use::text").get()
-
-    return shares_count
-    # comments_count - número de comentários que a notícia recebeu. Ex: 26
-    # summary - o primeiro parágrafo da notícia. Ex:"O CEO da Tesla, Elon Musk, garantiu que a montadora está muito perto de atingir o chamado nível 5 de autonomia de sistemas de piloto automático de carros. A informação foi confirmada em uma mensagem enviada pelo executivo aos participantes da Conferência Anual de Inteligência Artificial (WAIC, na sigla em inglês). O evento aconteceu em Xangai, na China, onde a montadora comemora resultados positivos de mercado."
-    # sources - lista contendo fontes da notícia. Ex: ["Venture Beat", "Source 2"]
-    # categories - lista de categorias que classificam a notícia. Ex: ["Mobilidade Urbana/Smart Cities", "Veículos autônomos", "Tesla", "Elon Musk"]
+    data["comments_count"] = check_comments(
+        select.css("#js-comments-btn ::attr(data-count)").get()
+    )
+    summary_list = select.css(
+        "div.tec--article__body > p:nth-child(1) ::text"
+    ).getall()
+    data["summary"] = "".join(summary_list)
+    data["sources"] = retirar_espaco(
+        select.css("div.z--mb-16 > div > a ::text").getall()
+    )
+    data["categories"] = retirar_espaco(
+        select.css("#js-categories *::text").getall()
+    )
+    return data
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    if not isinstance(amount, int):
+        return "Entrada invalida"
+
+    lista_url_noticias = []
+    screpe_fetch = fetch("https://www.tecmundo.com.br/novidades")
+    lista_url_noticias += scrape_novidades(screpe_fetch)
+
+    while len(lista_url_noticias) <= int(amount):
+        next_url = scrape_next_page_link(screpe_fetch)
+        next_page = fetch(next_url)
+        lista_url_noticias += scrape_novidades(next_page)
+
+    lista_dict_notice = []
+    for x in range(amount):
+        feth_noticia = fetch(lista_url_noticias[x])
+        noticia = scrape_noticia(feth_noticia)
+        lista_dict_notice.append(noticia)
+
+    create_news(lista_dict_notice)
+
+    return lista_dict_notice
