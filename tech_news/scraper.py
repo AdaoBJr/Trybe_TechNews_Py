7cf840a2d768885503d902d1168b054de1f6c6a0
brunamfguimaraes <brunamfguimaraes@gmail.com>
2022-01-12 14:04:03 -0300

    Iniciando projeto - Requisito 1 resolvido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        resp = requests.get(url, timeout=3)
+        if resp.status_code == 200:
+            return resp.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
