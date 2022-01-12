6a4219eca3fe4e7b2717cc73b8c826fbbcb62bcc
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-12 14:51:10 -0300

    requisito 7 completo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from tech_news.database import db
 
 
@@ -14,8 +15,19 @@ def search_by_title(title):
 
 
 # Requisito 7
+# https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    news_list = list(db.news.find({"timestamp": {"$regex": date}}))
+    result = []
+    for notice in news_list:
+        notice_tuple = (notice["title"], notice["url"])
+        result.append(notice_tuple)
+    return result
 
 
 # Requisito 8
