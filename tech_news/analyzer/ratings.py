5378fd3b38cd80f2677961bf713361fb371dcc17
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2022-01-12 17:40:07 -0300

    req. 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = find_news()
+    data = []
+
+    for notice in result:
+        notice["soma"] = notice["shares_count"] + notice["comments_count"]
+    
+    # Ordenação por soma - https://docs.python.org/3/howto/sorting.html
+    result.sort(key=lambda x: x["soma"], reverse=True)
+    top_five = result[:5]
+
+    for notice in top_five:
+        data.append((notice["title"], notice["url"]))
+
+    return data
 
 
 # Requisito 11
