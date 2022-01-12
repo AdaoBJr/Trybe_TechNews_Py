fe91264ec9b24bedfd6eaf9413fdb68d100a4904
Jonathan Souza <souzajonathan570@gmail.com>
2022-01-12 19:53:32 -0300

    req 04 concluido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.utils.utils import (
+    get_categories,
+    get_comments_count,
+    get_shares_count,
+    get_sources,
+    get_summary,
+    get_timestamp,
+    get_title,
+    get_url,
+    get_writer
+)
 
 
 # Requisito 1
@@ -21,10 +32,10 @@ def fetch(url):
 def scrape_novidades(html_content):
     selector = Selector(html_content)
     link = selector.css(
-        "div.tec--card__info " +
-        "h3.tec--card__title " +
-        "a.tec--card__title__link::attr(href)"
-        ).getall()
+        "div.tec--card__info "
+        + "h3.tec--card__title "
+        + "a.tec--card__title__link::attr(href)"
+    ).getall()
 
     return link
 
@@ -34,7 +45,7 @@ def scrape_next_page_link(html_content):
     selector = Selector(html_content)
     url = selector.css("a.tec--btn::attr(href)")
 
-    if (len(url) == 0):
+    if len(url) == 0:
         return None
 
     return url.get()
@@ -42,7 +53,31 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = get_url(selector)
+    title = get_title(selector)
+    timestamp = get_timestamp(selector)
+    writer = get_writer(selector)
+    shares_count = get_shares_count(selector)
+    comments_count = get_comments_count(selector)
+    summary = get_summary(selector)
+    sources = get_sources(selector)
+    categories = get_categories(selector)
+
+    new_dict = {}
+
+    new_dict["url"] = url
+    new_dict["title"] = title
+    new_dict["timestamp"] = timestamp
+    new_dict["writer"] = writer
+    new_dict["shares_count"] = shares_count
+    new_dict["comments_count"] = comments_count
+    new_dict["summary"] = summary
+    new_dict["sources"] = sources
+    new_dict["categories"] = categories
+
+    return new_dict
 
 
 # Requisito 5
---
+++ tech_news/utils/utils.py
@@ -0,0 +1,77 @@
+import re
+
+
+def get_url(selector):
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    return url
+
+
+def get_title(selector):
+    title = selector.css("h1.tec--article__header__title::text").get()
+    return title
+
+
+def get_timestamp(selector):
+    timestamp = selector.css("time::attr(datetime)").get()
+    return timestamp
+
+
+def get_writer(selector):
+    writer = selector.css(
+        "#js-author-bar >"
+        "div > p.z--m-none.z--truncate.z--font-bold >"
+        "a::text"
+    ).get()
+
+    if writer is None:
+        writer = selector.css(
+            "#js-main > div > article > div.tec--article__body-grid > " +
+            "div.z--pt-40.z--pb-24 > div.z--flex.z--items-center > "
+            "div.tec--timestamp.tec--timestamp--lg > " +
+            "div.tec--timestamp__item.z--font-bold > a::text"
+        ).get()
+
+    if writer is None:
+        writer = selector.css(
+            "#js-author-bar > div > p.z--m-none.z--truncate.z--font-bold::text"
+        ).get()
+
+    if writer is not None:
+        writer = writer.strip()
+
+    return writer
+
+
+def get_shares_count(selector):
+    shares_count = selector.css("div.tec--toolbar__item::text")
+
+    if len(shares_count) == 0:
+        shares_count = 0
+    else:
+        shares_count = re.findall(r"\d+", shares_count.get())[0]
+
+    return int(shares_count)
+
+
+def get_comments_count(selector):
+    comments_count = selector.css("button.tec--btn::attr(data-count)").get()
+    return int(comments_count)
+
+
+def get_summary(selector):
+    summary = selector.css(
+        "div.tec--article__body " "p:nth-child(1) *::text"
+    ).getall()
+    return "".join(summary)
+
+
+def get_sources(selector):
+    sources = selector.css("div.z--mb-16 > div > a.tec--badge::text").getall()
+
+    return [source.strip() for source in sources]
+
+
+def get_categories(selector):
+    categories = selector.css("div#js-categories > a::text").getall()
+
+    return [category.strip() for category in categories]
