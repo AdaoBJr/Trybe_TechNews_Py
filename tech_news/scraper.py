fe5c502cc2ada552f5d572ae29814699a97c1815
angelo <angelogpbl.info@gmail.com>
2022-01-12 14:07:09 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,19 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
