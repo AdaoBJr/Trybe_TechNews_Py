3380f517a6181a4ab34d3e1d2d309b771d1508e2
Huggo Parcelly <h.parcelly@gmail.com>
2022-01-12 12:08:19 -0300

    adiciona a funçao que busca a noticia pela data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -8,14 +9,30 @@ def search_by_title(title):
     news_list = []
     search_list = search_news(query)
     for news in search_list:
-        news_list.append((news['title'], news['url']))
+        news_list.append((news["title"], news["url"]))
 
     return news_list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    separate_date = date.split("-")
+    year = int(separate_date[0])
+    month = int(separate_date[1])
+    day = int(separate_date[2])
+    try:
+        datetime.datetime(year, month, day)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_query = f"^{date}"
+    query = {"timestamp": {"$regex": date_query}}
+    news_list = []
+    search_list = search_news(query)
+    for news in search_list:
+        news_list.append((news["title"], news["url"]))
+
+    return news_list
 
 
 # Requisito 8
