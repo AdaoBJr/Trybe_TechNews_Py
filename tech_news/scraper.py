e8fe57d23cbdb820ddb0b6b2328656b71319f532
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-12 17:33:39 -0300

    feat: scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,15 +27,109 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
-    result = selector.css(
-        ".tec--list.tec--list--lg > a ::attr(href)"
-    ).get()
+    result = selector.css(".tec--list.tec--list--lg > a ::attr(href)").get()
     return result
 
 
+def get_url(html_content):
+    selector = Selector(html_content)
+    result = selector.css("head > link[rel=canonical] ::attr(href)").get()
+    return result
+
+
+def get_title(html_content):
+    selector = Selector(html_content)
+    result = selector.css("#js-article-title ::text").get()
+    return result
+
+
+def get_timestamp(html_content):
+    selector = Selector(html_content)
+    result = selector.css("#js-article-date ::attr(datetime)").get()
+    return result
+
+
+def get_writer(html_content):
+    selector = Selector(text=html_content)
+    query = [
+        ".tec--timestamp__item.z--font-bold ::text",
+        ".tec--author__info__link ::text",
+        "div > p.z--m-none.z--truncate.z--font-bold::text",
+    ]
+
+    for i in query:
+        item = selector.css(i).get()
+        if item is not None:
+            return item.strip()
+    return None
+
+
+def get_shares_count(html_content):
+    selector = Selector(html_content)
+    shares_text = selector.css(
+        "js-author-bar > nav > div[class=tec--tool__item] ::text"
+    ).get()
+    if shares_text is not None:
+        shares_value = shares_text.strip().split(" ")[0]
+        return int(shares_value)
+    return 0
+
+
+def get_comments_count(html_content):
+    selector = Selector(html_content)
+    result = int(selector.css("#js-comments-btn ::attr(data-count)").get())
+    return result
+
+
+def get_summary(html_content):
+    selector = Selector(html_content)
+    summary = "".join(
+        selector.css(".p402_premium > p:nth-child(1) ::text").getall()
+    )
+    if summary is not None:
+        return summary
+    return None
+
+
+def get_sources(html_content):
+    selector = Selector(html_content)
+    query = [
+        ".z--mb-16 > div > a ::text",
+        ".z--mb-16.z--px-16 > div ::text",
+    ]
+
+    for i in query:
+        item_list = selector.css(i).getall()
+        if item_list is not []:
+            item_list_wo_space = list(map(lambda x: x.strip(), item_list))
+            return list(filter(lambda x: x != "", item_list_wo_space))
+    return None
+
+
+def get_categories(html_content):
+    selector = Selector(html_content)
+    categories = selector.css("#js-categories ::text").getall()
+    categories_wo_space = list(
+        map(lambda x: x.strip(), categories)
+    )
+    return list(filter(lambda x: x != "", categories_wo_space))
+
+
+# ajuda de Pedro Henrique Pires
+# PR: https://github.com/tryber/sd-010-b-tech-news/pull/5
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    scrape_dict = {}
+    scrape_dict["url"] = get_url(html_content)
+    scrape_dict["title"] = get_title(html_content)
+    scrape_dict["timestamp"] = get_timestamp(html_content)
+    scrape_dict["writer"] = get_writer(html_content)
+    scrape_dict["shares_count"] = get_shares_count(html_content)
+    scrape_dict["comments_count"] = get_comments_count(html_content)
+    scrape_dict["summary"] = get_summary(html_content)
+    scrape_dict["sources"] = get_sources(html_content)
+    scrape_dict["categories"] = get_categories(html_content)
+    return scrape_dict
 
 
 # Requisito 5
