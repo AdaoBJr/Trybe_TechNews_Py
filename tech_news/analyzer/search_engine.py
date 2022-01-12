028d13ae7f140f28be8b0fe4f3394d589dfbcb19
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-12 19:27:34 -0300

    Feat: Resolvendo requisito 6 do projeto Tech News

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import db
+import re
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    find_result = db.news.find({"title": re.compile(title, re.IGNORECASE)})    
+    list_title = []
+    for item in find_result:
+        list_title += [(item["title"], item["url"])]
+    return list_title
 
 
 # Requisito 7
