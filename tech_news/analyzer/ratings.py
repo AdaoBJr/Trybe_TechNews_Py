e646530a0f929150e782ed7b7c11b0e82bd39f9a
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-12 17:32:36 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,7 +1,37 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
-
+    pipeline = [
+        {"$project": {
+            "popularity": {"$sum": ["shares_count", "comments_count"]},
+            "title": True,
+            "url": True,
+        }},
+        {
+            "$sort": {
+                "popularity": -1,
+                "title": 1,
+            },
+        },
+        {
+            "$limit": 5,
+        },
+        {
+            "$project": {
+                "_id": False,
+                "title": True,
+                "url": True,
+            },
+        },
+    ]
+    news_list = list(db.news.aggregate(pipeline))
+    result = []
+    for notice in news_list:
+        notice_tuple = (notice["title"], notice["url"])
+        result.append(notice_tuple)
+    return result
 
 # Requisito 11
 def top_5_categories():
