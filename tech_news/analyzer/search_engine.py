b403194ccf417fb158bcd525942f218870bdbb64
angelo <angelogpbl.info@gmail.com>
2022-01-12 19:52:25 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    case_sensitive_title = title.capitalize()
+    news = search_news({"title": case_sensitive_title})
+    result = []
+    for n in news:
+        result.append((n["title"], n["url"]))
+    return result
 
 
 # Requisito 7
