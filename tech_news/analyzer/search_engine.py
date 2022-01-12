e1042d69089c6c5e6d938092e2a067bd024f69c1
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-12 20:50:18 -0300

    req 6 - feito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import db
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = db.news.find({"title": re.compile(title, re.IGNORECASE)})
+    news_list = []
+    for notice in news:
+        news_list += [(notice["title"], notice["url"])]
+    return news_list
 
 
 # Requisito 7
