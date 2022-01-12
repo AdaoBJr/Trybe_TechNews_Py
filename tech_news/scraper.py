443aa3bc3d427ea9434a5c47b728536e04c3befe
angelo <angelogpbl.info@gmail.com>
2022-01-12 16:25:26 -0300

    requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,6 +36,43 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head > link[rel=canonical] ::attr(href)").get()
+    title = selector.css("#js-article-title ::text").get()
+    timestamp = selector.css("#js-article-date ::attr(datetime)").get()
+    try:
+        writer = selector.css(".z--font-bold ::text").get().strip()
+    except AttributeError:
+        writer = ""
+    try:
+        shares_count = (
+            selector.css("#js-author-bar > nav > div:nth-child(1) ::text")
+            .get()
+            .strip()
+            .split(" ")[0]
+        )
+    except AttributeError:
+        shares_count = 0
+    comments_count = selector.css("#js-comments-btn ::attr(data-count)").get()
+    summary = selector.css(
+        "div.tec--article__body-grid >"
+        " div.tec--article__body > p:nth-child(1) ::text"
+    ).getall()
+
+    sources = selector.css(".z--mb-16 .tec--badge ::text").getall()
+    categories = selector.css("#js-categories > a ::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": "".join(summary),
+        "sources": [i.strip() for i in sources],
+        "categories": [i.strip() for i in categories],
+    }
 
 
 # Requisito 5
