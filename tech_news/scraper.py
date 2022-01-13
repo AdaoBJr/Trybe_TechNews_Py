ea264722a4773902adf68f35ed046cb793c6ed49
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-01-13 07:36:01 -0300

    Resolvido o requisito 02

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 # Requisito 1 - Iniciando o Projeto Tech News
 
 
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url_news = selector.css("div > h3 > a ::attr(href)").getall()
+    return url_news
 
 
 # Requisito 3
