cb8a1f2198d4b2a10e172b0560b1b7353b309dc9
hugolfsimoes <hugolfsimoes@gmail.com>
2022-01-12 21:03:33 -0300

    [X] 7 - Crie a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+import datetime
 # Requisito 6
 
 
@@ -12,8 +13,20 @@ def search_by_title(title):
 
 
 # Requisito 7
+# https://pythonhelp.wordpress.com/2011/11/12/fatiamento-slicing-de-strings-em-python/#:~:text=print%20s%5B%2D1%5D%20!,da%20string%20que%20queremos%20acessar.
+# https://qastack.com.br/programming/16870663/how-do-i-validate-a-date-string-format-in-python
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    result = []
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        for news in all_news:
+            if news["timestamp"][0:10] == date:
+                result.append((news["title"], news["url"]))
+        print(result)
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
