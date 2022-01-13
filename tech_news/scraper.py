d1d6c42db1abcc38b3d27adcb8bc4edd69a63ad8
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-13 19:32:29 -0300

    consertando relquisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,18 +2,23 @@ from parsel import Selector
 import time
 import requests
 
-
+regex_number = r"\d+"
 selector_url = "head > link[rel=canonical]::attr(href)"
 selector_title = "h1.tec--article__header__title::text"
 selector_timestamp = "div.tec--timestamp__item > time::attr(datetime)"
-selector_writer = "div.tec--author a.tec--author__info__link::text"
+selector_writer = ".tec--author a.tec--author__info__link::attr(href)"
+selector_writer_2 = ".tec--timestamp__item a::attr(href)"
 selector_shares_count = ".tec--toolbar > .tec--toolbar__item::text"
 selector_comments_count = ".tec--toolbar > "
 selector_comments_count += ".tec--toolbar__item > button::attr(data-count)"
-p_sumary = "string(/html/body/div[1]/main/div[1]/article/div[3]/div[2]/p[1])"
-p_sources = "/html/body/div[1]/main/div[1]/article/div[3]/div[4]/div/a/text()"
-path_categories = "/html/body/div[1]/main"
-path_categories += "/div[1]/article/div[3]/div[5]/div/a/text()"
+selector_sumary = "head > meta[name=description]::attr(content)"
+selector_sources = ".z--mb-16 div a::text"
+selector_categories = "#js-categories a::text"
+
+
+def rm_spaces_in_string(string):
+    if(string is not None):
+        return string[1:-1]
 
 
 def rm_spaces_in_array(array):
@@ -21,11 +26,6 @@ def rm_spaces_in_array(array):
     for string in array:
         result.append(string[1:-1])
     return result
-
-
-def rm_spaces_in_string(string):
-    if(string is not None):
-        return string[1:-1]
 
 
 # Requisito 1
@@ -56,18 +56,42 @@ def scrape_next_page_link(html_content):
     return link
 
 
+def scrape_writer(url_writer):
+    html = fetch(url_writer)
+    selector = Selector(text=html)
+    writer_name = selector.css(".tec--author-header__title::text").get()
+    return writer_name
+
+
+def get_writer(html_content):
+    selector = Selector(text=html_content)
+    link_writer_type1 = selector.css(selector_writer).get()
+    if(link_writer_type1):
+        return scrape_writer(link_writer_type1)
+
+    link_writer_type2 = selector.css(selector_writer_2).get()
+    if(link_writer_type2):
+        return scrape_writer(link_writer_type2)
+
+    writer_name = selector.css(".tec--author__info p:first-child::text").get()
+    if(writer_name):
+        return writer_name
+
+
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     url = selector.css(selector_url).get()
     title = selector.css(selector_title).get()
     timestamp = selector.css(selector_timestamp).get()
-    writer = rm_spaces_in_string(selector.css(selector_writer).get())
-    shares_count = int(selector.css(selector_shares_count).re_first(r"\d+") or 0)
+    writer = get_writer(html_content)
+    shares_count = int(
+        selector.css(selector_shares_count).re_first(regex_number) or 0
+    )
     comments_count = int(selector.css(selector_comments_count).get())
-    summary = selector.xpath(p_sumary).get()
-    sources = rm_spaces_in_array(selector.xpath(p_sources).getall())
-    categories = rm_spaces_in_array(selector.xpath(path_categories).getall())
+    summary = selector.css(selector_sumary).get()
+    sources = rm_spaces_in_array(selector.css(selector_sources).getall())
+    categories = rm_spaces_in_array(selector.css(selector_categories).getall())
     return{
         'url': url, 'title': title, 'timestamp': timestamp,
         'writer': writer, 'shares_count': (shares_count),
@@ -79,7 +103,3 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-html = fetch("https://www.tecmundo.com.br/minha-serie/215168-10-viloes-animes-extremamente-inteligentes.htm")
-print(scrape_noticia(html))
