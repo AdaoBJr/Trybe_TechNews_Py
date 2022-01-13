7021ab64c445d0f9049ae1a8e4ea2f5e91b179ce
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-12 21:18:04 -0300

    req 7 - feito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import db
 import re
+from datetime import datetime
 
 
 # Requisito 6
@@ -13,7 +14,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        find_date = list(db.news.find({"timestamp": {"$regex": date}}))
+
+        news_date = []
+        for notice in find_date:
+            news_date += [(notice["title"], notice["url"])]
+
+        return news_date
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
