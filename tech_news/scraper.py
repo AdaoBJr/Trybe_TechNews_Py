c3d102a6f7c73287dfca36d5eaac46479e005902
Gabriel <gabriel.mirandab92@gmail.com>
2022-01-12 22:25:46 -0300

    adding scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,46 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        selector = Selector(text=html_content)
+        # Referência:
+        # https://stackoverflow.com/questions/52849274/getting-the-current-url-page-ref-scrapy
+        url = selector.css("link[rel='canonical']::attr(href)").get()
+        title = selector.css(".tec--article__header__title::text").get()
+        timestamp = selector.css("#js-article-date::attr(datetime)").get()
+        writer = selector.css(".z--font-bold *::text").get().strip()
+        comments_count = selector.css(".tec--btn::attr(data-count)").get()
+        shares_count = (
+            selector.css(".tec--toolbar__item::text")
+            .get()
+            .lstrip()
+            .split()[:2][0]
+        )
+        # Referência:
+        # https://www.geeksforgeeks.org/python-string-join-method/
+
+    except AttributeError:
+        shares_count = 0
+
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+
+    categories = selector.css("#js-categories > a::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [s.strip() for s in sources],
+        "categories": [c.strip() for c in categories],
+    }
 
 
 # Requisito 5
