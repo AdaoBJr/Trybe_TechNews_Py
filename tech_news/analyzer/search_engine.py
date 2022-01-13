3bd97e95387909d523c3b42b033de6c2d3b9fa8b
Paulo Xavier <pxavierf@gmail.com>
2022-01-12 23:55:12 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+import re
+from tech_news.database import db
+
+# source
+# https://stackoverflow.com/questions/6266555/querying-mongodb-via-pymongo-in-case-insensitive-efficiently
+# https://www.tutorialspoint.com/python/python_dictionary.htm
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    list_news = db.news.find({"title": re.compile(title, re.IGNORECASE)})
+    list_title_find = []
+    for title_news in list_news:
+        list_title_find.append((title_news["title"], title_news["url"]))
+    return list_title_find
 
 
 # Requisito 7
