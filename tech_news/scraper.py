16bc22316f4f01e6a0516a943e6b8240b53ca7ed
Pedro Toselli <phtoselli@gmail.com>
2022-01-13 15:25:22 -0300

    Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,13 +1,13 @@
+from parsel import Selector
 import requests
 import time
-import parsel
 
 
 # Requisito 1
 def fetch(url):
     time.sleep(1)
     try:
-        response = requests.get(url, timeout=3)
+        response = requests.get(url, timeout=5)
         # https://docs.python-requests.org/en/latest/user/advanced/#timeouts
         response.raise_for_status()
     except (requests.HTTPError, requests.Timeout):
@@ -18,7 +18,7 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    selector = parsel.Selector(html_content)
+    selector = Selector(html_content)
     novidades = selector.css(
         ".tec--list__item .tec--card__title__link::attr(href)"
     ).getall()
@@ -28,21 +28,67 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    selector = parsel.Selector(html_content)
-    next_page_button_link = selector.css(
-        ".tec--btn::attr(href)"
-    ).get()
+    selector = Selector(html_content)
+    next_page_button_link = selector.css(".tec--btn::attr(href)").get()
 
     return next_page_button_link
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
-    pass
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title ::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+
+    writer = selector.css(".tec--author__info__link ::text").get() or ""
+
+    try:
+        shares = selector.css(".tec--toolbar__item ::text").get()
+        shares_count = shares.strip().split(" ")[0]
+    except AttributeError:
+        shares_count = 0
+
+    comments_count = (
+        selector.css("#js-comments-btn ::text").getall()[1].split()[0]
+    )
+
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css(".tec--badge--primary::text").getall()
+
+    # return categories
+
+    news_dict = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip(),
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
+
+    return news_dict
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
     pass
+
+
+if __name__ == "__main__":
+    print(
+        scrape_noticia(
+            fetch(
+                "https://www.tecmundo.com.br/mercado/231813-fim-delivery-do-uber-eats-ifood-tornou-monopolista-brasil.htm"
+            )
+        )
+    )
