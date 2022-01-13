8c2d2ac54e16e2978474ffeefc5ee8a1b2a6ab64
Paulo Xavier <pxavierf@gmail.com>
2022-01-13 00:29:52 -0300

    req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,12 @@
 import re
+import datetime
 from tech_news.database import db
 
 # source
 # https://stackoverflow.com/questions/6266555/querying-mongodb-via-pymongo-in-case-insensitive-efficiently
 # https://www.tutorialspoint.com/python/python_dictionary.htm
+# https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
+# https://docs.mongodb.com/manual/reference/operator/query/regex/
 
 
 # Requisito 6
@@ -17,7 +20,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    list_news = list(db.news.find({"timestamp": {"$regex": date}}))
+
+    list_title_find = []
+    for title_news in list_news:
+        list_title_find.append((title_news["title"], title_news["url"]))
+    return list_title_find
 
 
 # Requisito 8
