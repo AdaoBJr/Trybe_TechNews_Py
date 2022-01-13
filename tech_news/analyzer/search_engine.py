a143f5a79681c119ea67644934aa50821de8466f
Pedro Toselli <phtoselli@gmail.com>
2022-01-13 17:40:41 -0300

    Requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_finder = find_news()
+    news_list = []
+
+    for item in news_finder:
+        if item['title'].upper() == title.upper():
+            model = (item['title'], item['url'])
+            news_list.append(model)
+    return news_list
 
 
 # Requisito 7
@@ -16,3 +26,10 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+if __name__ == '__main__':
+    print(search_by_title('ok'))
+    # print(search_by_date())
+    # print(search_by_source())
+    # print(search_by_category())
