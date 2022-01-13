d3807bb02f035a6588541296b65a9c834544ab60
Bruno Wedley <brunowedley27@gmail.com>
2022-01-13 14:54:57 -0300

    correçoes na formataçao

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -17,18 +17,19 @@ def fetch(url):
         return None
 
 
-
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
     selector = parsel.Selector(html_content)
     return selector.css("h3.tec--card__title > a ::attr(href)").getall()
 
+
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
     selector = parsel.Selector(html_content)
     return selector.css("div.tec--list.tec--list--lg > a ::attr(href)").get()
+
 
 # Requisito 4
 def scrape_noticia(html_content):
