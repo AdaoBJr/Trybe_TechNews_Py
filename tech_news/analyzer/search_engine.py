2c6926bdc6c52802771f9d579ecd157914868c54
Fernando <fhomaia@gmail.com>
2022-01-14 00:29:56 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+import tech_news.database as db_connection
+from tech_news.database import db
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news = db_connection.search_news({"title": {"$regex": title, '$options': 'i'}})
+    filtered_news = []
+    if len(news):
+        for data in news:
+            filtered_news.append((data['title'], data['url']))
+        return filtered_news
+    return news
 
 
 # Requisito 7
