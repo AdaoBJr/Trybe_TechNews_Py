458ea0d93557666ee5b02ec98cdc20c0abab53d7
Camila Arruda <camilaam1802@gmail.com>
2022-01-15 11:28:37 -0300

    Requisito 4 completo

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,11 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = selector.css(
+        ".tec--list.tec--list--lg > a::attr(href)"
+        ).get()
+    return result
 
 
 # Requisito 4
