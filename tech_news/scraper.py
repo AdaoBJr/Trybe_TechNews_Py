4ed3fe7cddd21eb1d381136c6df246dda930a00b
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-15 06:43:15 -0300

    scrapdNews until writer

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,13 +29,40 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selectorCss = Selector(text=html_content)
-    nextLink = selectorCss.css("div.tec--list > a::attr(href)").get()
+    nextLink = selectorCss.css('div.tec--list > a::attr(href)').get()
     return nextLink
+
+
+def getWriter(writer, selectorCss):
+    if writer is not None:
+        return writer[1:-1]
+    if writer is None:
+        return selectorCss.css('.tec--author__info > p::text').get()
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selectorCss = Selector(text=html_content)
+    scrapedNews = {}
+
+    scrapedNews['url'] = selectorCss.css(
+        'head > link[rel=canonical]::attr(href)'
+    ).get().strip()
+
+    scrapedNews['title'] = selectorCss.css(
+        'h1.tec--acticle__header__title::text'
+    ).get()
+
+    scrapedNews['timestamp'] = selectorCss.css(
+        'div.tec--timestamp__item > time::attr(datetime)'
+    ).get()
+
+    scrapedNews['writer'] = getWriter(
+        selectorCss.css(
+            '.tec--article__body-grid .z--font-bold a::text').get(),
+        selectorCss)
+
+    return scrapedNews
 
 
 # Requisito 5
