b5dcc36a252bbde3af8e9e52869a9204ef4fbed9
Diego Serafim <diegoserafim1@gmail.com>
2022-01-15 16:08:03 -0300

    criada funcção buscar top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+import operator
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    if (notices):
+        for notice in notices:
+            notice["score"] = notice["shares_count"] + notice["comments_count"]
+        sort_notice = (
+            sorted(notices, key=operator.itemgetter("score"), reverse=True)
+        )
+        top_5 = sort_notice[:5]
+        return [(notice["title"], notice["url"]) for notice in top_5]
+    return []
 
 
 # Requisito 11
