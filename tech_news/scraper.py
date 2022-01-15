7d0c008c1c08c5b0fe284a75feb7cde1bbe61ec8
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-15 19:10:16 -0300

    requisito 5 completo

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,5 @@
 from parsel import Selector
+from tech_news.database import create_news
 import time
 import requests
 
@@ -6,19 +7,17 @@ regex_number = r"\d+"
 selector_url = "head > link[rel=canonical]::attr(href)"
 selector_title = "h1.tec--article__header__title::text"
 selector_timestamp = "div.tec--timestamp__item > time::attr(datetime)"
-selector_writer = ".tec--author a.tec--author__info__link::attr(href)"
-selector_writer_2 = ".tec--timestamp__item a::attr(href)"
+selector_writer = ".z--font-bold"
 selector_shares_count = ".tec--toolbar > .tec--toolbar__item::text"
-selector_comments_count = ".tec--toolbar > "
-selector_comments_count += ".tec--toolbar__item > button::attr(data-count)"
-selector_sumary = "head > meta[name=description]::attr(content)"
+selector_comments_count = ".tec--toolbar__item > button::attr(data-count)"
+selector_sumary = ".tec--article__body > p:nth-child(1) *::text"
 selector_sources = ".z--mb-16 div a::text"
 selector_categories = "#js-categories a::text"
 
 
 # Requisito 1
 def fetch(url):
-    time.sleep(1)
+    time.sleep(5)
 
     try:
         response = requests.get(url, timeout=3)
@@ -42,8 +41,8 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    link = selector.css("div.tec--list > a.tec--btn::attr(href)").get()
-    return link
+    page_link = selector.css("div.tec--list > a.tec--btn::attr(href)").get()
+    return page_link
 
 
 def rm_spaces_in_array(array):
@@ -53,40 +52,20 @@ def rm_spaces_in_array(array):
     return result
 
 
-def scrape_writer(url_writer):
-    html = fetch(url_writer)
-    selector = Selector(text=html)
-    writer_name = selector.css(".tec--author-header__title::text").get()
-    return writer_name
-
-
-def get_writer(html_content):
-    selector = Selector(text=html_content)
-    link_writer_type1 = selector.css(selector_writer).get()
-    if(link_writer_type1):
-        return scrape_writer(link_writer_type1)
-
-    link_writer_type2 = selector.css(selector_writer_2).get()
-    if(link_writer_type2):
-        return scrape_writer(link_writer_type2)
-
-    writer_name = selector.css(".tec--author__info p:first-child::text").get()
-    if(writer_name):
-        return writer_name
-
-
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     url = selector.css(selector_url).get()
     title = selector.css(selector_title).get()
     timestamp = selector.css(selector_timestamp).get()
-    writer = get_writer(html_content)
+    writer = (
+        selector.css(selector_writer).css("*::text").get().strip() or ""
+    )
     shares_count = int(
         selector.css(selector_shares_count).re_first(regex_number) or 0
     )
-    comments_count = int(selector.css(selector_comments_count).get())
-    summary = selector.css(selector_sumary).get()
+    comments_count = int(selector.css(selector_comments_count).get() or 0)
+    summary = "".join(selector.css(selector_sumary).getall())
     sources = rm_spaces_in_array(selector.css(selector_sources).getall())
     categories = rm_spaces_in_array(selector.css(selector_categories).getall())
     return{
@@ -104,4 +83,30 @@ def scrape_noticia(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    page_html = fetch("https://www.tecmundo.com.br/novidades")
+    news_links = []
+    result_news_scraped = []
+
+    news_links.extend(scrape_novidades(page_html))
+
+    counter = 0
+    while counter < amount:
+        page_news_html = {}
+
+        try:
+            page_news_html = fetch(news_links[counter])
+        except IndexError:
+            page_link = scrape_next_page_link(page_html)
+            page_html = fetch(page_link)
+            news_links.extend(scrape_novidades(page_html))
+            page_news_html = fetch(news_links[counter])
+
+        result_news_scraped.append(scrape_noticia(page_news_html))
+        # print(f"OBJETO {counter}:::", news_scraped[counter], "\n")
+        counter += 1
+
+    create_news(result_news_scraped)
+    return result_news_scraped
+
+
+# get_tech_news(10)
