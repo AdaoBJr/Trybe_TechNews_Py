dad149ccccedf72a9cca33d36b6487aba402e0cc
AdaoBJr <adaobjr@gmail.com>
2022-01-15 18:35:51 -0400

    feat: :sparkles: added Req07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,34 @@
 import tech_news.database as db
+from datetime import datetime
+
+
+def validate_date(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 6
 def search_by_title(title):
-    found_news = db.find_news()
+    find_news = db.find_news()
     return [
         (notice["title"], notice["url"])
-        for notice in found_news
+        for notice in find_news
         if title.lower() in notice["title"].lower()
     ]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+    found_news = db.find_news()
+    return [
+        (notice["title"], notice["url"])
+        for notice in found_news
+        if datetime.strptime(date, "%Y-%m-%d").date()
+        == datetime.strptime(notice["timestamp"], "%Y-%m-%dT%H:%M:%S").date()
+    ]
 
 
 # Requisito 8
