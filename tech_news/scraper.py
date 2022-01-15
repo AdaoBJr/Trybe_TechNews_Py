898f00c00ade7032f0247b45efba0424938f2577
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-15 05:31:46 -0300

    Req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        resp = requests.get(url, timeout=3)
+        resp.raise_for_status()
+    except (requests.HTTPError, requests.Timeout):
+        return None
+    else:
+        if (resp.status_code == 200):
+            return resp.text
 
 
 # Requisito 2
