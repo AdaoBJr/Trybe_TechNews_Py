c6d87cccc11568874681d29c9aa2a0216ebffe26
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-16 20:26:18 -0300

    correção

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -43,7 +43,8 @@ def search_by_source(source):
 
 # Requisito 9
 def search_by_category(category):
-    find_category = search_news({"categories": {'$regex': category, '$options': 'i'}})
+    find_category = search_news({
+        "categories": {'$regex': category, '$options': 'i'}})
     if find_category:
         for dic in find_category:
             tupla = [(dic["title"], dic["url"])]
