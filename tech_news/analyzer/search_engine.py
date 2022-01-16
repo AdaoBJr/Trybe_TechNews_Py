d6ff9fc2c0908dd518b20385fcb9b65cf0675ce2
Fernando <fhomaia@gmail.com>
2022-01-16 16:51:55 -0300

    req 7-9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import tech_news.database as db_connection
+from datetime import datetime
 
 
 # Requisito 6
@@ -10,27 +11,48 @@ def search_by_title(title):
     for data in news:
         filtered_news.append((data['title'], data['url']))
     return filtered_news
-   
+
+
+def check_date_format(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError('Data inválida')
+
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
-    # try:
-    #     query = {"timeStamp": ISODate(date)}
-    #     news = db_connection.search_news(query)
-    #     filtered_news = []
-    #     for data in news:
-    #         filtered_news.append((data['title'], data['url']))
-    #     return filtered_news
-    # except ValueError:
-    #     print('Data inválida')
+    check_date_format(date)
+
+    try:
+        query = {"timestamp": {"$regex": date}}
+        news = db_connection.search_news(query)
+        filtered_news = []
+        for data in news:
+            filtered_news.append((data['title'], data['url']))
+        return filtered_news
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    query = {"sources": {"$regex": source, '$options': 'i'}}
+    news = db_connection.search_news(query)
+    filtered_news = []
+    for data in news:
+        filtered_news.append((data['title'], data['url']))
+    return filtered_news
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    query = {"categories": {"$regex": category, '$options': 'i'}}
+    news = db_connection.search_news(query)
+    filtered_news = []
+    for data in news:
+        filtered_news.append((data['title'], data['url']))
+    return filtered_news
