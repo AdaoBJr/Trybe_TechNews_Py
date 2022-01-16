07d25e49d5033ffb1723fa1a73b25b7edc389fdb
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-16 07:42:22 -0300

    requisito 8, 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -3,14 +3,18 @@ from datetime import datetime
 import re
 
 
-# Requisito 6
-def search_by_title(title):
-    regx_title = re.compile(title, re.IGNORECASE)
-    array_news = list(db.news.find({"title": regx_title}))
+def format_news(array_news):
     result = []
     for news in array_news:
         result.append((news['title'], news['url']))
     return result
+
+
+# Requisito 6
+def search_by_title(title):
+    regx_title = re.compile(title, re.IGNORECASE)
+    array_news = list(db.news.find({"title": regx_title}))
+    return format_news(array_news)
 
 
 def is_valid_date(data):
@@ -27,20 +31,21 @@ def search_by_date(date):
 
     regx_date = re.compile(date, re.IGNORECASE)
     array_news = list(db.news.find({"timestamp": regx_date}))
-    result = []
-    for news in array_news:
-        result.append((news['title'], news['url']))
-    return result
+    return format_news(array_news)
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    regx_source = re.compile(source, re.IGNORECASE)
+    array_news = list(db.news.find({"sources": {"$in": [regx_source]}}))
+    return format_news(array_news)
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    regx_category = re.compile(category, re.IGNORECASE)
+    array_news = list(db.news.find({"categories": {"$in": [regx_category]}}))
+    return format_news(array_news)
 
 
 """ regex: https://stackoverflow.com/questions/3483318/
