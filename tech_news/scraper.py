9a5bea8616609d95d48e25eb2684e45871ae2881
Lucasrb <lucasrb04@gmail.com>
2022-01-16 10:16:42 -0300

    feat: :art: add fetch
    Adicionando a funçao fetch que retorna os dados do site, apos a raspagem

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        # recurso demora muito a responder
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            pass
+    except requests.ReadTimeout:
+        # Retornar none
+        pass
 
 
 # Requisito 2
