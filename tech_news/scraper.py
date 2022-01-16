57c411023735ca4e012a5d843a9bb3850e09415d
fabiohigoralm <fabiohigoralm2@gmail.com>
2022-01-16 12:30:19 -0300

    Requisito4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,35 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css(".z--font-bold *::text").get().strip()
+    shares = selector.css(".tec--toolbar__item::text").get()
+    if shares:
+        shares_count = shares.split()[0]
+    else:
+        shares_count = 0
+
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    su = selector.css(".tec--article__body > p:first-of-type *::text").getall()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    source = [i.strip() for i in sources]
+    categories = selector.css("#js-categories a::text").getall()
+    category = [i.strip() for i in categories]
+
+    return {
+        'url': url,
+        'title': title,
+        'timestamp': timestamp,
+        'writer': writer,
+        'shares_count': int(shares_count),
+        'comments_count': int(comments_count),
+        'summary': "".join(su),
+        "sources": source,
+        "categories": category,
+    }
 
 
 # Requisito 5
