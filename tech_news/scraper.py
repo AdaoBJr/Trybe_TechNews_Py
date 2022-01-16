e36d2ba9abb154bb85d76fce4bb4e2db82059028
Diegho <dieghonm@gmail.com>
2022-01-16 14:17:42 -0300

    Reqisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 from parsel import Selector
 
 
@@ -30,9 +31,53 @@ def scrape_next_page_link(html_content):
     return result
 
 
-# Requisito 4
+def shares_count_func(selector):
+    answer = selector.css("div.tec--toolbar__item::text").get()
+    if answer:
+        answer = int(re.findall(r'\d+', answer)[0])
+    else:
+        answer = 0
+    return answer
+
+
+# Requisito 4 [resolvido com ajuda da Letícia Galvão & Jonathan Souza]
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.tec--article__header__title::text").get()
+    timestamp = selector.css(
+        "div.tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = (
+        selector.css(".z--font-bold").css("*::text").get().strip() or ""
+    )
+    shares_count = shares_count_func(selector)
+    comments_count = selector.css(
+        "button#js-comments-btn::attr(data-count)"
+    ).get()
+
+    get_summary = selector.css(
+        "div.tec--article__body > p:nth-child(1) *::text"
+    ).getall()
+    summary = ''.join(get_summary)
+
+    get_sources = selector.css("div.z--mb-16 div a::text").getall()
+    sources = [i.strip() for i in get_sources]
+
+    get_categories = selector.css("div#js-categories a::text").getall()
+    categories = [i.strip() for i in get_categories]
+
+    return {
+        url,
+        title,
+        timestamp,
+        writer,
+        shares_count,
+        comments_count,
+        summary, sources,
+        categories
+    }
 
 
 # Requisito 5
