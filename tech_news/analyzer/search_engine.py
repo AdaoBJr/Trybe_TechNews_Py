d9f32eaf11c69a6b043b326f816bdc2b094fcb2a
Renato Graça <renatomacedo1995@gmail.com>
2022-01-16 19:30:23 -0300

    finish

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,62 @@
+from tech_news.database import search_news
+import datetime
+
+
+def valid_date(date):
+    try:
+        return datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    ZERO = 0
+    if len(response) > ZERO:
+        for result in response:
+            lists = [(result["title"], result["url"])]
+            return lists
+    return []
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    response = search_news({"timestamp": {"$regex": date}})
+    date_validation = valid_date(date)
+
+    if date_validation:
+        for result in response:
+            lists = [(result["title"], result["url"])]
+            return lists
+    return []
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    response = search_news(
+        {"sources": {"$elemMatch": {"$regex": source, "$options": "i"}}}
+    )
+
+    ZERO = 0
+    if len(response) > ZERO:
+        for result in response:
+            lists = [(result["title"], result["url"])]
+            return lists
+    return []
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    response = search_news(
+        {"categories": {"$elemMatch": {"$regex": category, "$options": "i"}}}
+    )
+
+    ZERO = 0
+    if len(response) > ZERO:
+        for result in response:
+            lists = [(result["title"], result["url"])]
+            return lists
+
+    return []
