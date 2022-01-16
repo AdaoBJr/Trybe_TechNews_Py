c67b4e4d928fff974a626d0f359ba0ec17e739ab
legalvao1 <legalvao1@gmail.com>
2022-01-16 08:59:14 -0300

    Requisito 10 - Funçao top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,23 @@
+from tech_news.database import find_news
+
+
+def myFunc(e):
+    return e['popularidade']
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = []
+    news_data = find_news()
+    # Source https://docs.python.org/pt-br/dev/howto/sorting.html
+    items = sorted(news_data, key=lambda new:
+                   new["shares_count"] + new["comments_count"],
+                   reverse=True)[:5]
+
+    for new in items:
+        item = new["title"], new["url"]
+        news_list.append(item)
+    return news_list
 
 
 # Requisito 11
