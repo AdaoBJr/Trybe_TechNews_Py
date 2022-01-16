62d10b8c4c08d6007e21e0ff7187aab7c25aca0f
Diegho <dieghonm@gmail.com>
2022-01-16 17:45:25 -0300

    Reqisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,10 @@
 from tech_news.database import db
+import datetime
 
 
 # Requisito 6
+# https://www.thecodebuzz.com/mongodb-query-case-sensitive-case-insensitive/
+# MongoDb regex query with case insensitive search
 def search_by_title(title):
     list = []
     for n in db.news.find({"title": {"$regex": title, "$options": "i"}}):
@@ -10,9 +13,27 @@ def search_by_title(title):
     return list
 
 
+# Leticia Galvão
+def valid_date(datestring):
+    try:
+        datetime.datetime.strptime(datestring, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if valid_date(date):
+        news = []
+        for n in db.news.find({
+            "timestamp": {"$regex": date, "$options": "i"}
+        }):
+            item = n["title"], n["url"]
+            news.append(item)
+        return news
+    else:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
