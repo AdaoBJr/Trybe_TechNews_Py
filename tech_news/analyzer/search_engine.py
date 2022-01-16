6c0124e408cf6f36c21505d7e021f741808821fb
Camila Arruda <camilaam1802@gmail.com>
2022-01-16 17:04:48 -0300

    requisito 6 completo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """https://www.thecodebuzz.com/mongodb-query-case-sensitive-case-
+    insensitive/"""
+    results = search_news(
+        {"title": {'$regex': title, '$options': 'i'}})
+
+    zero = 0
+    if len(results) > zero:
+        for result in results:
+            tupla_result = [(result['title'], result['url'])]
+            return tupla_result
+    return []
 
 
 # Requisito 7
