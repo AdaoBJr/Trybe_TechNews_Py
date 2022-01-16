f785c9d2ff8a8e13606256b699e71f8f202523fd
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-16 17:04:28 -0300

    Requisito finais

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -4,11 +4,11 @@ import datetime
 
 # Requisito 6
 def search_by_title(title):
-    query = {"title": title.lower()}
+    query = {"title": {"$regex": title, "$options": "i"}}
 
     result = search_news(query)
 
-    tupla_title_url = [(item.title, item.url) for item in result]
+    tupla_title_url = [(item["title"], item["url"]) for item in result]
 
     return tupla_title_url
 
@@ -17,35 +17,37 @@ def search_by_title(title):
 def search_by_date(date):
     try:
         datetime.datetime.strptime(date, "%Y-%m-%d")
-        query = {"timestamp": {"$regex": date}}
+        query = {"timestamp": {"$regex": date, "$options": "i"}}
 
         result = search_news(query)
 
-        tupla_title_url = [(item.title, item.url) for item in result]
+        tupla_title_url = [(item["title"], item["url"]) for item in result]
 
         return tupla_title_url
     except ValueError:
-        return "Data inválida"
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 # https://petrim.com.br/blog/index.php/2018/08/22/consultando-dentro-de-arrays-no-mongodb/
 def search_by_source(source):
-    query = {"sources": {"$elemMatch": {"$eq": source.lower()}}}
+    query = {"sources": {"$elemMatch": {"$regex": source, "$options": "i"}}}
 
     result = search_news(query)
 
-    tupla_title_url = [(item.title, item.url) for item in result]
+    tupla_title_url = [(item["title"], item["url"]) for item in result]
 
     return tupla_title_url
 
 
 # Requisito 9
 def search_by_category(category):
-    query = {"categories": {"$elemMatch": {"$eq": category.lower()}}}
+    query = {
+        "categories": {"$elemMatch": {"$regex": category, "$options": "i"}}
+    }
 
     result = search_news(query)
 
-    tupla_title_url = [(item.title, item.url) for item in result]
+    tupla_title_url = [(item["title"], item["url"]) for item in result]
 
     return tupla_title_url
