1cfa09458c534d03e07ce81f264b803b5f1dc933
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-16 08:29:21 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,33 @@
+from tech_news.database import db
+from tech_news.utils import format_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    array_news = list(db.news.aggregate(
+        [
+            {
+                "$project": {
+                    "title": "$title",
+                    "url": "$url",
+                    "popularity": {
+                        "$sum": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {
+                "$sort": {
+                    "popularity": -1,
+                    "title": 1
+                }
+            },
+            {
+                "$limit": 5
+            }
+        ]
+    ))
+
+    return format_news(array_news)
 
 
 # Requisito 11
