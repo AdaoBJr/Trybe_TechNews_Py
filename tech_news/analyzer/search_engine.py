5435fc8c1714d5fe9d4c35e7d548566a82732b9c
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-16 14:36:33 -0300

    finalizamos obrigado a todos que me ajudaram diretamente e indiretamente

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -39,10 +39,35 @@ def search_by_date(date):
 def search_by_source(source):
     """Seu código deve vir aqui"""
     notice_list = find_news()
-    print(notice_list, source)
+    # print(notice_list, source)
+    title_url_list = []
 
+    for notice in notice_list:
+        for fonte in notice["sources"]:
+            if fonte.upper() == source.upper():
+                title_url_list.append((notice["title"], notice["url"]))
+
+    return title_url_list
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    notice_list = find_news()
+    # print(notice_list, source)
+    title_url_list = []
+
+    for notice in notice_list:
+        for categ in notice["categories"]:
+            if categ.upper() == category.upper():
+                title_url_list.append((notice["title"], notice["url"]))
+
+    return title_url_list
+
+    return title_url_list
+# fontes do projeto
+# https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
+# https://docs.python.org/pt-br/3/library/datetime.html#examples-of-usage-datetime
+# https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
+# https://devhints.io/css
+# Menções honrosas! Repositórios de jonathan, henrique zozimo, leticia galvao
