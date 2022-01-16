89e57adb576d6e85360e3e4381e1480df259b384
dennisMarcelo <dennismarcelo15@gmail.com>
2022-01-15 23:29:35 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,20 +1,36 @@
 from tech_news.database import db
+from datetime import datetime
 import re
 
 
 # Requisito 6
 def search_by_title(title):
-    regx = re.compile(title, re.IGNORECASE)
-    array_news = list(db.news.find({"title": regx}))
+    regx_title = re.compile(title, re.IGNORECASE)
+    array_news = list(db.news.find({"title": regx_title}))
     result = []
     for news in array_news:
         result.append((news['title'], news['url']))
     return result
 
 
+def is_valid_date(data):
+    try:
+        datetime.strptime(data, '%Y-%m-%d')
+        return True
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    is_valid_date(date)
+
+    regx_date = re.compile(date, re.IGNORECASE)
+    array_news = list(db.news.find({"timestamp": regx_date}))
+    result = []
+    for news in array_news:
+        result.append((news['title'], news['url']))
+    return result
 
 
 # Requisito 8
@@ -25,3 +41,10 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+""" regex: https://stackoverflow.com/questions/3483318/
+performing-regex-queries-with-pymongo """
+
+""" date: https://pt.stackoverflow.com/questions/377579/
+valida%C3%A7%C3%A3o-de-data-testes-com-python """
