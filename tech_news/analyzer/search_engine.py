44ad12eb695902edf0593d961630f8fee87febbd
Lara Capila <laracapilak@gmail.com>
2022-01-16 17:34:05 -0300

    feat(requisito 6): Passando localmente

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    searching_for_all_news = find_news()
+    result = []
+
+    for new in searching_for_all_news:
+        # usei o lower pra padronizar a pesquisa pois as noticias vem com
+        # letra maiúsula
+        if new["title"].lower() == title.lower():
+            result.append((new["title"], new["url"]))
+    return result
 
 
 # Requisito 7
