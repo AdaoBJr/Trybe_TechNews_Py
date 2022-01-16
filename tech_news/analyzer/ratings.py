11d4aa70b3610ee79f98a89442516b3f845fbd43
fabiohigoralm <fabiohigoralm2@gmail.com>
2022-01-16 15:40:35 -0300

    Requisito10 e 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    # https://docs.python.org/3/howto/sorting.html
+    news = find_news()
+    for new in news:
+        new["popularity"] = new["shares_count"] + new["comments_count"]
+    sorted_news = sorted(news, key=lambda new: new["popularity"], reverse=True)
+    return [(new["title"], new["url"]) for new in sorted_news[:5]]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    aux_categories_list = []
+    for new in news:
+        aux_categories_list += new['categories']
+        print(aux_categories_list)
+    aux_categories_list.sort()
+    return aux_categories_list[:5]
