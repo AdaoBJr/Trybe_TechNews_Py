d0b17228a1bc0e861f8f34ff53059a44776bab12
Camila Arruda <camilaam1802@gmail.com>
2022-01-16 15:44:03 -0300

    Requisito 4 completo

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,6 +3,85 @@ import time
 from parsel import Selector
 
 
+"""Requisito 4 feito com a ajuda do Eder Paiva e do Jonathan Souza"""
+
+
+def url_results(selector):
+    link = selector.css(
+        "link[rel=canonical]::attr(href)"
+        ).get()
+    return link
+
+
+def title_results(selector):
+    title = selector.css(
+        "h1.tec--article__header__title::text"
+        ).get()
+    return title
+
+
+def timestamp_results(selector):
+    timestamp = selector.css(
+        "div.tec--timestamp__item time::attr(datetime)"
+        ).get()
+    return timestamp
+
+
+def writer_results(selector):
+    writer = selector.css(
+        "a.tec--author__info__link::text"
+        ).get()
+    if writer is None:
+        writer = selector.css(
+            "div.tec--timestamp div:nth-child(2) a::text").get()
+        if writer is None:
+            writer = selector.css("p.z--m-none::text").get()
+    return writer.strip()
+
+
+def shares_count_results(selector):
+    shares_count = selector.css(
+        "div.tec--toolbar__item::text"
+        ).get()
+    """ Vai pegar somente os numeros(string) e transformar em um INT """
+    if shares_count:
+        shares_count = shares_count.replace('Compartilharam', '')
+        shares_count = int(shares_count.strip())
+    else:
+        shares_count = 0
+    return shares_count
+
+
+def comment_count_results(selector):
+    comment_count = selector.css(
+        "button.tec--btn::attr(data-count)"
+        ).get()
+    return int(comment_count)
+
+
+def summary_results(selector):
+    summary = selector.css(
+        "div.tec--article__body > p:nth-child(1) *::text"
+        ).getall()
+    return "".join(summary)
+
+
+def sources_results(selector):
+    sources = selector.css(
+        "div.z--mb-16 > div > a.tec--badge::text"
+        ).getall()
+    """ retira os espaços desnecessarios e faz um for nas fontes """
+    return [source.strip() for source in sources]
+
+
+def categories_results(selector):
+    categories = selector.css(
+        "div#js-categories a::text"
+        ).getall()
+
+    return [category.strip() for category in categories]
+
+
 # Requisito 1
 def fetch(url):
     try:
@@ -36,7 +115,32 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    link = url_results(selector)
+    title = title_results(selector)
+    timestamp = timestamp_results(selector)
+    writer = writer_results(selector)
+    shares_count = shares_count_results(selector)
+    comments = comment_count_results(selector)
+    summary = summary_results(selector)
+    sources = sources_results(selector)
+    category = categories_results(selector)
+
+    """ criar o objeto para armazenar as informaçções """
+    obj_news = {}
+
+    """atribuir as chaves e valor relacionadas as informações das função aux"""
+    obj_news["url"] = link
+    obj_news["title"] = title
+    obj_news["timestamp"] = timestamp
+    obj_news["writer"] = writer
+    obj_news["shares_count"] = shares_count
+    obj_news["comments_count"] = comments
+    obj_news["summary"] = summary
+    obj_news["sources"] = sources
+    obj_news["categories"] = category
+
+    return obj_news
 
 
 # Requisito 5
