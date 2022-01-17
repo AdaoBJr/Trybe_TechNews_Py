32f8afb70a53213ea0e0c14a4550039a08d3eb43
Antenor <antenorzapata@gmail.com>
2022-01-17 11:21:08 -0300

    feat: add search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+# Based on:
+# https://stackoverflow.com/a/3483399/14362230
+# https://stackoverflow.com/a/48693451/14362230
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    return [(val["title"], val["url"]) for val in search_news({"title": {
+        "$regex": f"{title}", "$options": "i"
+        }})]
 
 
 # Requisito 7
