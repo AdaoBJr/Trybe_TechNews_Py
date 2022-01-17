277aaffbae7ca323ea68329483a023913cf6b814
isTorres <ismaeltorres612@gmail.com>
2022-01-16 22:16:17 -0300

    feat: req's 7,8,9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,41 +1,9 @@
+import datetime
 from ..database import find_news
 
 
 # Requisito 6
-# def low_str(s):
-#     return s.upper()
-
-
-# def mod_values(d, mod):
-#     d.upadte(title=mod(d["title"]))
-#     return d
-
-
-# def low_values_list(normal_list):
-#     print(normal_list, 'lsit')
-#     new_list = []
-#     for item in normal_list:
-#         low_item = mod_values(item, low_str)
-#         new_list.append(upp_item)
-#     return new_list
-
-
 def search_by_title(title):
-    # find_news()
-    # news_update = low_values_list(find_news())
-
-    # print(news_update, 'news')
-    # for notice in news_update:
-    #     insert_or_update(notice)
-    # result = []
-    # news_full = search_news({"title": low_str(title)})
-
-    # if len(news_full) < 1:
-    #     return []
-    # else:
-    #     for new in news_full:
-    #         result.append((new["title"], new["url"]))
-    #     return result
     data = find_news()
     result = []
     for notice in data:
@@ -46,14 +14,39 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    result = []
+
+    try:
+        datetime.date.fromisoformat(date)
+        for notice in data:
+            if notice["timestamp"].split("T")[0] == date:
+                result.append((notice["title"], notice["url"]))
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
+# https://stackoverflow.com/questions/1801668/convert-a-list-with-strings-all-to-lowercase-or-uppercase
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    result = []
+    for notice in data:
+        for i in range(len(notice["sources"])):
+            notice["sources"][i] = notice["sources"][i].lower()
+            if notice["sources"][i] == source.lower():
+                result.append((notice["title"], notice["url"]))
+    return result
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    result = []
+    for notice in data:
+        for i in range(len(notice["categories"])):
+            notice["categories"][i] = notice["categories"][i].lower()
+            if notice["categories"][i] == category.lower():
+                result.append((notice["title"], notice["url"]))
+    return result
