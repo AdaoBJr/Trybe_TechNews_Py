5c137a763702eca45641986da54a24263f5865f9
Lucasrb <lucasrb04@gmail.com>
2022-01-17 00:39:28 -0300

    req8 e 9 - search_by_source/category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,14 @@
-from tech_news.database import db
+from tech_news.database import find_news
 
 # Requisito 6
 
 
 def search_by_title(title):
     filtered_list = []
-    news_list = db.news.find({"title": {"$regex": title, "$options": "i"}})
+    news_list = find_news()
     for news in news_list:
-        filtered_list.append((news["title"], news["url"]))
+        if title.lower() in news["title"].lower():
+            filtered_list.append((news["title"], news["url"]))
     return filtered_list
 
 
@@ -18,9 +19,23 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    filtered_list = []
+    result = find_news()
+    for news in result:
+        for i in range(len(news["sources"])):
+            news["sources"][i] = news["sources"][i].lower()
+        if source.lower() in news["sources"]:
+            filtered_list.append((news["title"], news["url"]))
+    return filtered_list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    filtered_list = []
+    result = find_news()
+    for news in result:
+        for i in range(len(news["categories"])):
+            news["categories"][i] = news["categories"][i].lower()
+        if category.lower() in news["categories"]:
+            filtered_list.append((news["title"], news["url"]))
+    return filtered_list
