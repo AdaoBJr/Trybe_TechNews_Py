42dd181f453dddb3477cfef58297b52b80293962
André Arnoni <andre_arnoni@yahoo.com.br>
2022-01-16 23:23:39 -0300

    executados requisitos 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,31 @@
+import requests
+from parsel import Selector
+import time
+from requests.exceptions import ReadTimeout
+from tech_news.database import create_news
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        request = requests.get(url, timeout=3)
+        if request.status_code != 200:
+            return None
+        return request.text
+    except ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    if html_content == "":
+        return []
+    seletor = Selector(html_content).css(
+        'h3 .tec--card__title__link::attr(href)').getall()
+    return seletor
 
 
 # Requisito 3
