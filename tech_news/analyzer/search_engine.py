7d8b94dee03b3ef076fb999c9cfe9eec2fb8e073
Pedro Toselli <phtoselli@gmail.com>
2022-01-17 11:10:14 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+import datetime
 from tech_news.database import find_news
 
 
@@ -15,12 +16,32 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news_finder = find_news()
+    news_list = []
+    try:
+        datetime.date.fromisoformat(date)
+        #  https://docs.python.org/3/library/datetime.html
+        for item in news_finder:
+            if item['timestamp'][:10] == date:
+                news_list.append((item['title'], item['url']))
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    pass
+    # news_finder = find_news()
+    # news_list = []
+
+    # for item in news_finder:
+    #     formated_list = map(lambda x: x.lower(), item['sources'])
+    #     # https://www.delftstack.com/pt/howto/python/python-lowercase-list/
+    #     formated_source = source.lower()
+    #     if formated_source in formated_list:
+    #         news_list.append((item['title'], item['url']))
+    # return news_list
 
 
 # Requisito 9
@@ -29,7 +50,8 @@ def search_by_category(category):
 
 
 if __name__ == '__main__':
-    print(search_by_title('ok'))
-    # print(search_by_date())
-    # print(search_by_source())
+    pass
+    # print(search_by_title('ok'))
+    # print(search_by_date('2020-11-23'))
+    print(search_by_source('ResetEra'))
     # print(search_by_category())
