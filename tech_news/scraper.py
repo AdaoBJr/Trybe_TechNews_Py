b48060f4b0b8d2a482fb01611b603a1307522e86
Renan0211 <renan.antunes.rocha@gmail.com>
2022-01-17 18:28:24 -0300

    creating the fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+    else:
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
 
 
 # Requisito 2
