4bcfedfaa527a8ebb923b607d8aac00bb4223db2
Bruno Wedley <brunowedley27@gmail.com>
2022-01-17 15:59:27 -0300

    adiciona req 4 e 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import parsel
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -21,7 +22,7 @@ def fetch(url):
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
     selector = parsel.Selector(html_content)
-    return selector.css("h3.tec--card__title > a ::attr(href)").getall()
+    return selector.css("h3.tec--card__title > a::attr(href)").getall()
 
 
 # Requisito 3
@@ -34,8 +35,53 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    # url, title, timestamp (da noticia),
+    # writer, shares_count, comments_count,
+    # summary, sources, catergories
+    selector = parsel.Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title ::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    writer = selector.css(".z--font-bold *::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    comments_count = selector.css("#js-comments-btn ::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body > p:nth-child(1) ::text"
+        ).getall()
+    sources = selector.css(".z--mb-16 .tec--badge ::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip() if writer else None,
+        "shares_count": int(shares_count.split()[0]) if shares_count else 0,
+        "comments_count": int(comments_count) if comments_count else 0,
+        "summary": "".join(summary),
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    url_fetch = fetch(url)
+    news_scrape = scrape_novidades(url_fetch)
+    news_list = []
+
+    while len(news_scrape) < amount:
+        url = scrape_next_page_link(url_fetch)
+        url_fetch = fetch(url)
+        news_scrape.append(scrape_novidades(url_fetch))
+
+    for news in news_scrape:
+        fetch_news = fetch(news)
+        scraped_news = scrape_noticia(fetch_news)
+        news_list.append(scraped_news)
+
+    create_news(news_list)
+    return news_list
