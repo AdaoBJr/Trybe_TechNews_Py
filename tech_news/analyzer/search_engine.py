0f86a951c60fc8fec83278cce3ee47004c13e213
Aremtior <marcosleandrofl@gmail.com>
2022-01-18 10:46:11 -0300

    requirement 7 - search_by_date function was created

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,36 @@
+from datetime import datetime
 import re
 from tech_news.database import (
     search_news,
 )
 
 
+# 3
+def validate(date_text):
+    try:
+        if date_text != datetime.strptime(date_text, "%Y-%m-%d").strftime(
+            "%Y-%m-%d"
+        ):
+            raise ValueError
+        return True
+    except ValueError:
+        return False
+
+
 # Requisito 6
 def search_by_title(title):
     regx = re.compile(".*(%s).*" % title, re.IGNORECASE)  # 1, 2
     data = search_news({"title": regx})
-    return [(news['title'], news['url']) for news in data]
+    return [(news["title"], news["url"]) for news in data]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if not validate(date):
+        raise ValueError("Data inválida")
+    regx = re.compile("^(%s)" % date, re.IGNORECASE)  # 1, 2
+    data = search_news({"timestamp": regx})
+    return [(news["title"], news["url"]) for news in data]
 
 
 # Requisito 8
@@ -31,4 +48,7 @@ def search_by_category(category):
 https://stackoverflow.com/questions/24637917/how-to-add-a-variable-into-my-re-compile-expression
 # 2:
 https://stackoverflow.com/questions/3483318/performing-regex-queries-with-pymongo
+# 3:
+https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
+
 """
