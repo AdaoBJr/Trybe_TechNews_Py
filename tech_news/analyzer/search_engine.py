a465b5fc344f7131fdf6cfb34e30c90b39b56220
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-01-17 23:12:44 -0300

    feat(search_engine): search_by_date implementation

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 # Requisito 6
+import datetime
 from tech_news.database import search_news
 
 
@@ -13,8 +14,25 @@ def search_by_title(title):
 
 
 # Requisito 7
+# https://www.codevscolor.com/date-valid-check-python
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    year, month, day = [int(date_element) for date_element in date.split("-")]
+    isValidDate = True
+    try:
+        datetime.date(year, month, day)
+    except ValueError:
+        isValidDate = False
+
+    if isValidDate is False:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": {"$regex": date}}
+    search_result = search_news(query)
+    return (
+        [(news["title"], news["url"]) for news in search_result]
+        if len(search_result) > 0
+        else []
+    )
 
 
 # Requisito 8
@@ -25,3 +43,7 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+""" a = search_by_date("2020-11-23")
+print(a) """
