0159abe1f7ac4805661f96d1eaf10e3e2104d926
andredebem <andrebemferraz@gmail.com>
2022-01-18 19:10:03 -0300

    Request III

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_url = selector.css("div.tec--list__item ~ a::attr(href)").get()
+    return next_page_url or None
 
 
 # Requisito 4
