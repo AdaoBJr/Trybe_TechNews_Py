80bcbc8f9ef6657b525f8306a8357f12b89464cd
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-18 15:42:40 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,14 +33,55 @@ def scrape_novidades(html_content):
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
     res = selector.css(
-        "#js-main > div > div > div.z--col.z--w-2-3 > div.tec--list.tec--list--lg > a::attr(href)"
+        "#js-main > div > div > div.z--col.z--w-2-3 >"
+        " div.tec--list.tec--list--lg > a::attr(href)"
     )
     return res.get()
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+
+    title = selector.css("#js-article-title::text").get()
+
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+
+    writer = selector.css(".z--font-bold").css("*::text").get().strip() or ""
+
+    try:
+        shares_count = (
+            selector.css(".tec--toolbar__item::text")
+            .get()
+            .strip()
+            .split(" ")[0]
+        )
+    except AttributeError:
+        shares_count = 0
+
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+
+    categories = selector.css("#js-categories > a::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
