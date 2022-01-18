57fb1a604d00a175c91ec92a5674c20f0c221c73
Luiz Paulo Lima <luiz.lima@comuniki.me>
2022-01-18 04:08:57 -0300

    search_by_date search_by_source

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -14,12 +15,34 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    found_news = find_news()
+    try:
+        datetime.strptime(date, "%Y-%m-%d").date()
+        result = []
+        for new in found_news:
+            if (
+                datetime.strptime(date, "%Y-%m-%d").date()
+                == datetime.strptime(
+                    new["timestamp"], "%Y-%m-%dT%H:%M:%S"
+                ).date()
+            ):
+                content = (new["title"], new["url"])
+                result.append(content)
+            return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    found_news = find_news()
+    result = []
+    for new in found_news:
+        for new_source in new["sources"]:
+            if new_source.lower() == source.lower():
+                content = (new["title"], new["url"])
+                result.append(content)
+    return result
 
 
 # Requisito 9
