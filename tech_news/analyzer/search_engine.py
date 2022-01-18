b0aa2a8645d47d7ba569da5251bb7c830bbb6c8e
andersontrkz <trkz.anderson@gmail.com>
2022-01-18 19:51:17 -0300

    feat: req6 req7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,28 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+# BASED ON
+# https://docs.python.org/3/library/datetime.html
+# https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
+
+
+def search(field, text):
+    news_list = search_news({field: {"$regex": text, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in news_list]
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return search("title", title)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.fromisoformat(date).timestamp()
+    except ValueError:
+        raise ValueError("Data inválida")
+    return search("timestamp", date)
 
 
 # Requisito 8
