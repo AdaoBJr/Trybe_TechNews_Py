d634893f1ddc03c3b92c23d69dd98e167045fa0d
Maiara Borsatti <maiara.borsatti@gmail.com>
2022-01-18 15:33:04 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,9 @@
 from tech_news.database import find_news
+import datetime
 
 
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
     response = find_news()
     search = []
     for new in response:
@@ -14,7 +14,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    response = find_news()
+    search = []
+    try:
+        # https://docs.python.org/3/library/datetime.html
+        datetime.date.fromisoformat(date)
+        for new in response:
+            if new["timestamp"][0:10] == date:
+                search.append((new["title"], new["url"]))
+        return search
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
