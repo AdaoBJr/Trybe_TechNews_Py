e5be7cc5ad775e455a86eb9a1032f78805cbf3d8
walterlbf <walter.lbf.f@gmail.com>
2022-01-18 02:50:53 -0400

    funcao da paginaçao passando local

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,6 +33,12 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css(
+        "#js-main > div > div > div.z--col.z--w-2-3 >"
+        + " div.tec--list.tec--list--lg > a::attr(href)"
+    ).get()
+    return next_page_link
 
 
 # Requisito 4
