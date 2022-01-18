ac75bcf8369b0062c7b47c629060da437893b3e9
Renan0211 <renan.antunes.rocha@gmail.com>
2022-01-18 18:17:33 -0300

    implementing the scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,29 @@ import requests
 import time
 from parsel import Selector
 
+# HELPER FUNCTIONS
+
+
+def get_writer(selector):
+    writer = selector.css(
+        'a[href^="https://www.tecmundo.com.br/autor/"]::text'
+    ).getall()
+    if len(writer) > 1:
+        return selector.css(
+            'a[class][href^="https://www.tecmundo.com.br/autor/"]::text'
+        ).get()
+    try:
+        return writer[0]
+    except IndexError:
+        return selector.css(".tec--author__info p::text").get()
+
+
+def strip_list(list):
+    stripped_list = []
+    for item in list:
+        stripped_list.append(item.strip())
+    return stripped_list
+
 
 # Requisito 1
 def fetch(url):
@@ -37,6 +60,53 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+
+    title = selector.css(".tec--article__header__title::text").get()
+
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    writer = get_writer(selector=selector)
+
+    try:
+        stripped_writer = writer.strip()
+    except AttributeError:
+        stripped_writer = writer
+
+    shares_and_comments_count = selector.css(".tec--toolbar__item").re(
+        "^[0-9]*$"
+    )
+    try:
+        shares_count = shares_and_comments_count[0] or 0
+        comments_count = shares_and_comments_count[1] or 0
+    except IndexError:
+        shares_count = 0
+        comments_count = 0
+
+    summary = selector.css(
+        ".tec--article__body p:first-child *::text"
+    ).getall()
+
+    sources = selector.css(".z--mb-16 div a::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+
+    stripped_sources = strip_list(sources)
+    stripped_categories = strip_list(categories)
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": stripped_writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": "".join(summary),
+        "sources": stripped_sources,
+        "categories": stripped_categories,
+    }
 
 
 # Requisito 5
