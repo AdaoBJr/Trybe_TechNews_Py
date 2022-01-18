9eddefd29b176059854f86eea7f1cff34bd731c7
andersontrkz <trkz.anderson@gmail.com>
2022-01-18 16:14:05 -0300

    feat: req01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,27 @@
+import requests
+from time import sleep
+
+# BASED ON *
+# https://www.w3schools.com/python/ref_requests_get.asp
+# https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+# https://github.com/tryber/sd-10b-live-lectures/blob/lecture/34.3/main_scraper.py
+# *
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    response = ""
+    try:
+        sleep(1)
+        response = requests.get(url, timeout=3)
+
+    except requests.exceptions.RequestException:
+        pass
+
+    finally:
+        if response != "" and response.status_code == 200:
+            return response.text
+        pass
 
 
 # Requisito 2
