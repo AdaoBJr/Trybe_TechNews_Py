c64f87048c0545a84976b535eeabced247ef6d55
Luiz Paulo Lima <luiz.lima@comuniki.me>
2022-01-18 03:13:49 -0300

    scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,8 +36,74 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+# https://github.com/tryber/sd-010-b-tech-news/pull/47/files
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    url = (
+        Selector(text=html_content)
+        .css("link[rel=canonical]::attr(href)")
+        .get()
+    )
+
+    title = Selector(text=html_content).css("#js-article-title ::text").get()
+
+    timestamp = (
+        Selector(text=html_content)
+        .css("#js-article-date ::attr(datetime)")
+        .get()
+    )
+
+    try:
+        writer = (
+            Selector(text=html_content)
+            .css(".z--font-bold")
+            .css("*::text")
+            .get()
+            .strip()
+            or ""
+        )
+    except AttributeError:
+        writer = None
+
+    try:
+        shares_count = (
+            Selector(text=html_content)
+            .css(".tec--toolbar__item::text")
+            .get()
+            .strip()
+            .split(" ")[0]
+        )
+    except AttributeError:
+        shares_count = 0
+
+    comments_count = (
+        Selector(text=html_content).css(".tec--btn::attr(data-count)").get()
+    )
+
+    summary = "".join(
+        Selector(text=html_content)
+        .css(".tec--article__body > p:first-child *::text")
+        .getall()
+    )
+
+    sources = (
+        Selector(text=html_content).css(".z--mb-16 .tec--badge::text").getall()
+    )
+
+    categories = (
+        Selector(text=html_content).css(".tec--badge--primary::text").getall()
+    )
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
