51cee0a6f37f7b869ca5afd115af283fdc37a76b
Misael Bueno <misa.hbueno@gmail.com>
2022-01-18 20:30:13 -0300

    req7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -13,12 +14,24 @@ def search_by_title(title):
 
     return list_news
 
-    # print(result[0])
-
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        news = find_news()
+        date_search = datetime.strptime(date, '%Y-%m-%d').date()
+        list_news = []
+
+        for new in news:
+            array_date = new['timestamp'].split("T")
+            date_news = datetime.strptime(array_date[0], '%Y-%m-%d').date()
+            if date_search == date_news:
+                list_news.append((new['title'], new['url']))
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    return list_news
 
 
 # Requisito 8
