015f7459b034b4bda784cc8c2af447ab0446664e
walterlbf <walter.lbf.f@gmail.com>
2022-01-19 00:56:01 -0400

    requisito 7 passando local

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+import datetime
 
 
 # Requisito 6
@@ -12,8 +13,27 @@ def search_by_title(title):
 
 
 # Requisito 7
+# verifica se a data esta no formato correto
+def valid_date(datestring):
+    try:
+        datetime.datetime.strptime(datestring, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    if valid_date(date):
+        news = []
+        for n in db.news.find({
+            "timestamp": {"$regex": date, "$options": "i"}
+        }):
+            item = n["title"], n["url"]
+            news.append(item)
+        return news
+    else:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
