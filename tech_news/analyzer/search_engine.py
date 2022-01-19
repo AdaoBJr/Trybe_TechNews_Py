c9bd7e3893148794b38a0772015eef3aeda8c745
Felippe017 <felippecorrea2010@gmail.com>
2022-01-18 21:38:15 -0300

    DONE req6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_database = find_news()
+    new_list = []
+
+    for new in news_database:
+        if new["title"].lower() == title.lower():
+            new_list.append((new["title"], new["url"]))
+    return new_list
 
 
 # Requisito 7
