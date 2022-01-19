facf22c3a54b8cbf1f8dd6082da29de231b8ad1b
Rafael <rmathscoach@gmail.com>
2022-01-18 21:53:27 -0300

    remove comentários

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -42,11 +42,6 @@ def search_by_source(source):
 
 
 # Requisito 9
-# The function must receive the full category name as a parameter.
-# The function should fetch the news from the database by category.
-# The function must return in the same format as the previous requirement.
-# If no news is found, an empty list must be returned.
-# The search must be case insensitive
 def search_by_category(category):
     category_news = {
         "categories": {"$regex": category, "$options": "i"}
