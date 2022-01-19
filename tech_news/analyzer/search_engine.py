a163a44d2e6f043ab87bb05d7d3ec5d1495ab5a2
Renan Braga <renanpbraga@gmail.com>
2022-01-19 00:01:10 -0200

    adiciona req 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,6 +2,13 @@ from tech_news.database import find_news
 import datetime
 
 
+# Agradecimento aos amigos:
+# Felippe Correa,
+# João Herculano
+# Rafael Mathias e
+# Lucas Lotar
+# pela companhia e auxílio
+
 # Requisito 6
 def search_by_title(title):
     news_found = find_news()
@@ -28,9 +35,21 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_found = find_news()
+    news_list = []
+    for news in news_found:
+        for index in news["sources"]:
+            if index.lower() == source.lower():
+                news_list.append((news["title"], news["url"]))
+    return news_list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_found = find_news()
+    news_list = []
+    for news in news_found:
+        for index in news["categories"]:
+            if index.lower() == category.lower():
+                news_list.append((news["title"], news["url"]))
+    return news_list
