5a79b8af61e7ec9708138b1bc3d0792eb1694a82
Gildeir <gildeirlopes@gmail.com>
2022-01-19 00:49:05 -0300

    tech_news/analyzer/search_engine.py
    tech_news/scraper.py

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,41 @@
+from tech_news.database import find_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response = find_news()
+    search = []
+    for new in response:
+        if new["title"].lower() == title.lower():
+            search.append((new["title"], new["url"]))
+    return search
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    response = find_news()
+    search = []
+    try:
+        datetime.date.fromisoformat(date)
+        for new in response:
+            if new["timestamp"][0:10] == date:
+                search.append((new["title"], new["url"]))
+        return search
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    found_news = find_news()
+    result = []
+    for new in found_news:
+        for new_source in new["sources"]:
+            if new_source.lower() == source.lower():
+                content = (new["title"], new["url"])
+                result.append(content)
+    return result
 
 
 # Requisito 9
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -40,9 +41,90 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    url = (
+        Selector(text=html_content)
+        .css("link[rel=canonical]::attr(href)")
+        .get()
+    )
+
+    title = Selector(text=html_content).css("#js-article-title ::text").get()
+
+    timestamp = (
+        Selector(text=html_content)
+        .css("#js-article-date ::attr(datetime)")
+        .get()
+    )
+
+    try:
+        writer = (
+            Selector(text=html_content)
+            .css(".z--font-bold")
+            .css("*::text")
+            .get()
+            .strip()
+            or ""
+        )
+    except AttributeError:
+        writer = None
+
+    try:
+        shares_count = (
+            Selector(text=html_content)
+            .css(".tec--toolbar__item::text")
+            .get()
+            .strip()
+            .split(" ")[0]
+        )
+    except AttributeError:
+        shares_count = 0
+
+    comments_count = (
+        Selector(text=html_content).css(".tec--btn::attr(data-count)").get()
+    )
+
+    summary = "".join(
+        Selector(text=html_content)
+        .css(".tec--article__body > p:first-child *::text")
+        .getall()
+    )
+
+    sources = (
+        Selector(text=html_content).css(".z--mb-16 .tec--badge::text").getall()
+    )
+
+    categories = (
+        Selector(text=html_content).css(".tec--badge--primary::text").getall()
+    )
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    html_content = fetch(url)
+    news = scrape_novidades(html_content)
+    news_list = []
+
+    while len(news) < amount:
+        url = scrape_next_page_link(html_content)
+        html_content = fetch(url)
+        news.extend(scrape_novidades(html_content))
+
+    for new_url in news[0:amount]:
+        response = fetch(new_url)
+        new = scrape_noticia(response)
+        news_list.append(new)
+
+    create_news(news_list)
+    return news_list
