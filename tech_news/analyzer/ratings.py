89a0dccdb4dee6e392cd28439ad45c27dd4b9a6a
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2022-01-19 03:42:09 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_formated = []
+    res = []
+    for new in news:
+        sum_amount = new["shares_count"] + new["comments_count"]
+        news_formated.append((new["title"], new["url"], sum_amount))
+
+    news_formated.sort(key=lambda tup: tup[2], reverse=True)
+
+    for new in news_formated:
+        res.append((new[0], new[1]))
+    return res[0:5]
 
 
 # Requisito 11
