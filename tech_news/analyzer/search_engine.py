d29f5e54656b8e724e95b417d84b567491e91f46
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-19 09:32:01 -0300

    req7 - implementa funçao search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,33 @@
 from tech_news.database import find_news
+import datetime
 
 
+# FONTE lower(): https://shortest.link/2z5G
 # Requisito 6
 def search_by_title(title):
     search_news = find_news()
     news_by_title = []
     for new in search_news:
         if new['title'].lower() == title.lower():
-            news_by_title.append((new['title'], new['url']))
+            item = new["title"], new["url"]
+            news_by_title.append(item)
     return news_by_title
 
 
+# FONTE strptime(): encr.pw/Pf9PS
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    search_news = find_news()
+    news_by_date = []
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        for new in search_news:
+            if new['timestamp'][:10] == date:
+                item = new["title"], new["url"]
+                news_by_date.append(item)
+        return news_by_date
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
