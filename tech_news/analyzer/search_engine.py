623faaf081a86a9ce474f7892d005fadcd11c95e
esionascimento <esio.392@protonmail.com>
2022-01-19 06:07:23 -0400

    req 06 ate 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,42 @@
+from tech_news.database import db
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    list_news = []
+    news = db.news.find({"title": {"$regex": title, "$options": "i"}})
+    for i in news:
+        list_news.append((i["title"], i["url"]))
+    return list_news
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    new = []
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    news = db.news.find({"timestamp": {"$regex": date}})
+    for curNews in news:
+        new.append((curNews["title"], curNews["url"]))
+    return new
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    list_titles = []
+    news = db.news.find({"sources": {"$regex": source, "$options": "i"}})
+    for curNews in news:
+        list_titles.append((curNews["title"], curNews["url"]))
+    return list_titles
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    data_by_category = []
+    news = db.news.find({"categories": {"$regex": category, "$options": "i"}})
+    for curNews in news:
+        data_by_category.append((curNews["title"], curNews["url"]))
+    return data_by_category
