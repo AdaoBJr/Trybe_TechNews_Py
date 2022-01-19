674ae7f98bad561eb55484e928c075178f865779
walterlbf <walter.lbf.f@gmail.com>
2022-01-19 00:48:24 -0400

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import db
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    list = []
+    for new in db.news.find({"title": {"$regex": title, "$options": "i"}}):
+        item = new["title"], new["url"]
+        list.append(item)
+    return list
 
 
 # Requisito 7
