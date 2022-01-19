f007b18c78c094e516cd6915eefae243d5416d0d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-19 09:12:26 -0300

    req 7 e 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,10 @@
-from tech_news.database import find_news
+from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
-    news = find_news()
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
     news_list = []
 
     for report in news:
@@ -15,12 +16,28 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news = search_news({"timestamp": {"$regex": date}})
+    news_list = []
+    try:
+        datetime.strptime(date, "%Y-%m-%d")  # Documentação do Python
+        for report in news:
+            news_list.append((report["title"], report["url"]))
+        return news_list
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = search_news(
+        {"sources": {"$elemMatch": {"$regex": source, "$options": "i"}}}
+    )
+    news_list = []
+
+    for report in news:
+        news_list.append((report["title"], report["url"]))
+    return news_list
 
 
 # Requisito 9
