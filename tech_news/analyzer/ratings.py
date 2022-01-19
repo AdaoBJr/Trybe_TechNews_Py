8710e25ecb6eb70589b7159b78580a745673d651
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2022-01-19 09:07:37 -0300

    ops

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,26 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news = [
+        (
+            new["title"],
+            new["url"],
+            new["shares_count"] + new["comments_count"],
+        )
+        for new in news
+    ]
+    # https://docs.python.org/pt-br/dev/howto/sorting.html
+    top_news = sorted(news, key=lambda new: new[2], reverse=True)
+    return [(new[0], new[1]) for new in top_news][:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news = [category for new in news for category in new["categories"]]
+    return sorted(news, key=lambda new: new)[:5]
+     
+
--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,39 @@
+import re
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = set()
+    # https://stackoverflow.com/questions/500864/case-insensitive-regular-expression-without-re-compile
+    news = search_news({"title": re.compile(title, re.IGNORECASE)})
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news = set()
+    try:
+        # https://docs.python.org/3/library/datetime.html
+        datetime.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
+        # https://stackoverflow.com/questions/9978534/match-dates-using-python-regular-expressions/9978701
+    news = search_news({"timestamp": re.compile(date)})
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = set()
+    news = search_news({"sources": re.compile(source, re.IGNORECASE)})
+    return [(new["title"], new["url"]) for new in news]    
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = set()
+    news = search_news({"categories": re.compile(category, re.IGNORECASE)})
+    return [(new["title"], new["url"]) for new in news]
+    
