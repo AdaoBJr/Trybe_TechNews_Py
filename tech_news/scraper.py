846662d3d3e3981796bf4f616dd6d20861b872a3
antonioarieiro <sfgfffs@gmail.com>
2022-01-19 11:04:09 -0300

    selec html content and get url by html.css href

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,3 +40,18 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    news = []
+    URL = "https://www.tecmundo.com.br/novidades"
+    while len(news) < amount:
+        page = fetch(URL)
+        links = scrape_novidades(page)
+        for link in links:
+            fetch_page = fetch(link)
+            scrape_page = scrape_noticia(fetch_page)
+            if len(news) == amount:
+                break
+            news.append(scrape_page)
+        URL = scrape_next_page_link(page)
+    create_news(news)
+
+    return news
