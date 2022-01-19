689c45a571cd9e55e7399ad0a7f53a9da2632607
andredebem <andrebemferraz@gmail.com>
2022-01-18 21:49:10 -0300

    Request IV

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 
 from parsel import Selector
 
@@ -38,7 +39,56 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    current_url_page = selector.xpath('//link[@rel="canonical"]/@href').get()
+
+    title = selector.css("h1.tec--article__header__title::text").get()
+
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+
+    writer_css = selector.css(".z--font-bold *::text").get() or None
+    if writer_css:
+        writer = writer_css.strip()
+    else:
+        writer = writer_css
+
+    toolbar_item = selector.css("div.tec--toolbar__item::text").get()
+    if toolbar_item:
+        shares_count = int(re.search(r"\d+", toolbar_item)[0])
+    else:
+        shares_count = 0
+
+    comments_count = int(
+        selector.css(".tec--toolbar__item button::attr(data-count)").get()
+    )
+
+    list_summary = selector.css(
+        "div.tec--article__body p:nth-child(1) *::text"
+    ).getall()
+    summary = ""
+    for element in list_summary:
+        summary += element
+
+    sources = selector.css(".z--mb-16 div a::text").getall()
+    sources_stripped = [source.strip() for source in sources]
+
+    categories = selector.css("#js-categories a::text").getall()
+    categories_stripped = [category.strip() for category in categories]
+
+    news_dict = {
+        "url": current_url_page,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources_stripped,
+        "categories": categories_stripped,
+    }
+
+    return news_dict
 
 
 # Requisito 5
