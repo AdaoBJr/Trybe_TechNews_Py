0a1c117faab88dd9b8e97229a20ab93786235096
antonioarieiro <sfgfffs@gmail.com>
2022-01-20 14:04:38 -0300

    req 11 return list new

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,22 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
 
 
+def sorted_value(val):
+  top_val = val.sort()
+  return top_val[:5]
+
+
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    empyt = []
+    top_categories = []
+    list_news = find_news()
+    return list_news
+
