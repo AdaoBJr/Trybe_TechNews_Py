551a8d1e37140dfbe97ac7f61869ed20793c0d3c
antonioarieiro <sfgfffs@gmail.com>
2022-01-21 12:27:27 -0300

    fix lint

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -22,16 +22,16 @@ case = {
 
 
 def analyzer_menu():
-    select_menu = input("""
-        Selecione uma das opções a seguir:\n
-        0 - Popular o banco com notícias;\n
-        1 - Buscar notícias por título;\n
-        2 - Buscar notícias por data;\n
-        3 - Buscar notícias por fonte;\n
-        4 - Buscar notícias por categoria;\n
-        5 - Listar top 5 notícias;\n
-        6 - Listar top 5 categorias;\n
-        7 - Sair."""
+    select_menu = input(
+        "Selecione uma das opções a seguir:"
+        "0 - Popular o banco com notícias;\n"
+        "1 - Buscar notícias por título;\n"
+        "2 - Buscar notícias por data;\n"
+        "3 - Buscar notícias por fonte;\n"
+        "4 - Buscar notícias por categoria;\n"
+        "5 - Listar top 5 notícias;\n"
+        "6 - Listar top 5 categorias;\n"
+        "7 - Sair."
     )
     calls = {
         "0": lambda: int(input("Digite quantas notícias serão buscadas:")),
