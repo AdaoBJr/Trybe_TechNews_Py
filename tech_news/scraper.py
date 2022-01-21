9f904cf0e41d6f6b9c389790eaf97bf61a57b5ae
fernandacajueiro <fernandacajueiromachado@gmail.com>
2022-01-21 18:26:12 -0300

    req 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,10 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    tag = selector.css('div.tec--list')
+    news = tag.css('.tec--card__thumb__link::attr(href)').getall()
+    return news
 
 
 # Requisito 3
