385e9af1425e17cf5c581f44ec57ece164f9c9f7
antonioarieiro <sfgfffs@gmail.com>
2022-01-21 10:31:01 -0300

    end req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -7,23 +7,27 @@ def top_5_news():
     """Seu código deve vir aqui"""
     list_news = find_news()
     # return list_news
-    for a in list_news:
-        # calcular a sua "popularidade"
+    if(list_news):
+        for a in list_news:
+          # calcular a sua "popularidade"
         # somando seu número de compartilhamentos e comentários.
-        sum = a["shares_count"] + a["comments_count"]
-        a["popularity"] = sum
-        # print(a)
-    # A função deve ordenar as notícias por ordem de popularidade.
-    # Em caso de empate, o desempate deve ser por ordem alfabética de título.
-    list_news = sorted(list_news, key=lambda x: x["title"])
-    list_news = sorted(list_news, key=lambda x: x["popularity"], reverse=True)
-    # A função deve ter retorno no mesmo formato do requisito anterior,
-    # porém limitado a 5 notícias.
-    five = [list_news for list_news in list_news[:5]]
-    my_arr = []
-    for a in five:
-        my_arr.append(tuple([a["title"], a["url"]]))
-    return my_arr
+            sum = a["shares_count"] + a["comments_count"]
+            a["popularity"] = sum
+            # print(a)
+        # A função deve ordenar as notícias por ordem de popularidade.
+        # Em caso de empate, o desempate deve ser por ordem alfabética de título.
+        list_news = sorted(list_news, key=lambda x: x["title"])
+        list_news = sorted(list_news, key=lambda x: x["popularity"], reverse=True)
+        # A função deve ter retorno no mesmo formato do requisito anterior,
+        # porém limitado a 5 notícias.
+        five = [list_news for list_news in list_news[:5]]
+        my_arr = []
+        for a in five:
+            my_arr.append(tuple([a["title"], a["url"]]))
+        return my_arr
+    else:
+      # Caso não haja notícias disponíveis, deve-se retornar uma lista vazia.
+        return []
 
 
 # Requisito 11
