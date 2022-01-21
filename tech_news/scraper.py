fd6be0b09447fdfe0604acb70b63196f3eacfd13
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2022-01-21 10:38:27 -0300

    Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,9 +29,45 @@ def scrape_next_page_link(html_content):
     return element
 
 
+# Requisito feito com a ajuda do João
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        element = Selector(html_content)
+        url = element.css("link[rel='canonical']::attr(href)").get()
+        title = element.css(".tec--article__header__title::text").get()
+        date = element.css("#js-article-date::attr(datetime)").get()
+        write = element.css(".z--font-bold *::text").get().strip()
+        counter_comments = element.css(".tec--btn::attr(data-count)").get()
+        counter_share = (
+            element.css(".tec--toolbar__item::text")
+            .get()
+            .lstrip()
+            .split()[:2][0]
+        )
+
+    except AttributeError:
+        counter_share = 0
+
+    summary = "".join(
+        element.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+
+    sources = element.css(".z--mb-16 .tec--badge::text").getall()
+    categories = element.css("#js-categories > a::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": date,
+        "writer": write,
+        "shares_count": int(counter_share),
+        "comments_count": int(counter_comments),
+        "summary": summary,
+        "sources": [source.strip() for source in sources],
+        "categories": [category.strip() for category in categories],
+    }
 
 
 # Requisito 5
