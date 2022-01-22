d579f6d9c78e11509b7b1c010f6150bd28d9e2cd
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-22 16:30:16 -0500

    Trying too do the tenth requirement

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    bd_news = find_news()
+    news_with_ratings = []
+    for new in bd_news:
+        rating = new["shares_count"] + new["comments_count"]
+        news_with_ratings.append((new["title"], new["url"], rating))
+    news_with_ratings.sort(key=lambda news_tup: news_tup[2], reverse=True)
+    rated_news = []
+    for new in news_with_ratings:
+        rated_news.append((new[0], new[1]))
+    return rated_news[0:5]
 
 
 # Requisito 11
