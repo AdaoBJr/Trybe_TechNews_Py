35c9e04c234137254c3da6f2fc88a6fb5c1b699e
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-22 09:37:50 -0500

    Trying too do the fourth requirement

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,36 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(
+        "h1.tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)").get()
+    writer = selector.css(".tec--author__info a::text").get() or None
+    shares_count = selector.css(
+        "div.tec--toolbar__item::text").get() or 0
+    shares_count = shares_count.strip(" Compartilharam ")
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body > p:nth-child(1) *::text").getall()
+    summary_text = ""
+    for sum in summary:
+        summary_text = summary_text + sum
+    sources = selector.css(".z--mb-16 a.tec--badge::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary_text,
+        "sources": sources,
+        "categories": categories
+    }
 
 
 # Requisito 5
