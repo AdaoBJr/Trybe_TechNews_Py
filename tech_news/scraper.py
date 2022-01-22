8124fc37e13f98447538f057aa4093957747b01e
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-21 23:31:19 -0500

    Trying too do the second requirement - return an empty list

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -17,7 +17,7 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    if html_content == "":
+    if html_content == "" or html_content == None:
         return list()
     selector = Selector(text=html_content)
     novidades_urls_list = selector.css(".tec--card__info h3 a::attr(href)")
