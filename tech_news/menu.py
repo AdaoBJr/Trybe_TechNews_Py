ee5686372a88873e092205fb4f305102233451f1
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-22 18:02:09 -0500

    Trying too do the twelveth requirement

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -3,7 +3,15 @@ import sys
 
 # Requisito 12
 def analyzer_menu():
-    option_selected = input("Selecione uma das opções a seguir:\n 0 - Popular o banco com notícias;\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n 3 - Buscar notícias por fonte;\n 4 - Buscar notícias por categoria;\n 5 - Listar top 5 notícias;\n 6 - Listar top 5 categorias;\n 7 - Sair.")
+    option_selected = input("""Selecione uma das opções a seguir:
+  0 - Popular o banco com notícias;
+  1 - Buscar notícias por título;
+  2 - Buscar notícias por data;
+  3 - Buscar notícias por fonte;
+  4 - Buscar notícias por categoria;
+  5 - Listar top 5 notícias;
+  6 - Listar top 5 categorias;
+  7 - Sair.""")
 
     answers = {
         "0": "Digite quantas notícias serão buscadas:",
