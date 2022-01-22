180dc252c884004f3aab52f2dcb254e9f39025ea
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-21 22:59:15 -0500

    Trying too do the first requirement

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=5)
+    except requests.ReadTimeout:
+        return None
+    finally:
+        if response.status_code != 200:
+            return None
+        return response.text
 
 
 # Requisito 2
