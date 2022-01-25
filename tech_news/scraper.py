46ae2cebd44c6fe72434ee8e93b7210fa8d84355
Abimael <abimaelr.albu@gmail.com>
2022-01-25 18:02:08 -0300

    fix

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -6,10 +6,13 @@ from parsel import Selector
 # Requisito 1
 def fetch(url):
     time.sleep(1)
-    request = requests.get(url)
-    if request.status_code == 200:
-        return request.text
-    return None
+    try:
+        request = requests.get(url, timeout=5)
+        if request.status_code == 200:
+            return request.text
+        return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
@@ -17,8 +20,6 @@ def scrape_novidades(html_content):
     site_selector = Selector(text=html_content)
     novidades = (
         site_selector.css('.tec--card__title__link::attr(href)').getall())
-    if(html_content == ''):
-        return []
     return novidades
 
 
@@ -26,7 +27,7 @@ def scrape_novidades(html_content):
 def scrape_next_page_link(html_content):
     site_selector = Selector(text=html_content)
     next_page_link = (
-        site_selector.css('a .tec--btn::attr(href)').get())
+        site_selector.css('a.tec--btn::attr(href)').get())
     print(next_page_link)
     if next_page_link == '':
         return None
